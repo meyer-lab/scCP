cefb7460df6eceb7e18d745030c34019a5de9848
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2022-05-09 16:37:19 -0700

    Pt cp factors (#51)
    * PlottingFactors
    
    * Fix iterations
    
    * PlottingNK
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- gmm/figures/figure4.py
+++ gmm/figures/figure4.py
@@ -2,6 +2,8 @@
 This creates Figure 4.
 """
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import tensorly as tl
 from scipy.optimize import minimize
 from jax.config import config
@@ -9,7 +11,8 @@ from jax import value_and_grad
 from .common import subplotLabel, getSetup
 from gmm.imports import smallDF
 from gmm.GMM import probGMM
-from gmm.tensor import tensor_decomp, tensorcovar_decomp, cp_pt_to_vector, maxloglik_ptnnp
+from gmm.tensor import tensor_decomp, tensorcovar_decomp, cp_pt_to_vector, maxloglik_ptnnp, vector_to_cp_pt
+from tensorly.cp_tensor import cp_normalize
 
 
 config.update("jax_enable_x64", True)
@@ -18,7 +21,7 @@ config.update("jax_enable_x64", True)
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((10, 8), (2, 2))
+    ax, f = getSetup((10, 8), (2, 3))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -55,5 +58,24 @@ def makeFigure():
     opt = minimize(func, totalVector, jac=True, method="L-BFGS-B", args=args, options={"iprint": 50, "maxiter": 1000})
 
     tl.set_backend("numpy")
+
+    rebuildCpFactors, rebuildPtFactors, rebuildPtCore = vector_to_cp_pt(opt.x[facInfo.shape[0]::], facInfo.rank, facInfo.shape)
+    maximizedCpInfo = cp_normalize(rebuildCpFactors)
+
+    ax[0].bar(np.arange(1,maxcluster+1),opt.x[0:facInfo.shape[0]])
+    xlabel = "Cluster"
+    ylabel = "NK Value"
+    ax[0].set(xlabel=xlabel, ylabel=ylabel)
+
+
+    cmpCol = [f"Cmp. {i}" for i in np.arange(1, ranknumb + 1)]
+
+    maximizedFactors = []
+    for ii, dd in enumerate(tMeans.dims):
+        maximizedFactors.append(pd.DataFrame(maximizedCpInfo.factors[ii], columns=cmpCol, index=tMeans.coords[dd]))
+
+    for i in range(0, len(facInfo.shape)):
+        heatmap = sns.heatmap(data= maximizedFactors[i], ax=ax[i+1], vmin=0, vmax=1, cmap="Blues")
+
 
     return f
