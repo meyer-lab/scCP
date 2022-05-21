e4ecb0b966ee2eb9fa84b4d116a77bbdb5b6f1db
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2022-05-20 20:04:57 -0700

    Pt patterns (#58)
    * PrecisionMatrixPatterns
    
    * FixingGMMTest
    
    * Changing maxiter
    
    * Fix Fig. 4
    
    * Fix Fig. 5
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>
    Co-authored-by: Aaron Meyer <ameyer@ucla.edu>

--- gmm/figures/figure4.py
+++ gmm/figures/figure4.py
@@ -2,11 +2,12 @@
 This creates Figure 4.
 """
 import numpy as np
+import pandas as pd
 import seaborn as sns
 from jax.config import config
 from .common import subplotLabel, getSetup
 from gmm.imports import smallDF
-from gmm.tensor import minimize_func
+from gmm.tensor import minimize_func, markerslist
 
 
 config.update("jax_enable_x64", True)
@@ -15,7 +16,7 @@ config.update("jax_enable_x64", True)
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((10, 8), (2, 3))
+    ax, f = getSetup((10, 8), (3, 3))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -24,15 +25,21 @@ def makeFigure():
     # Final Xarray has dimensions [Marker, Cell Number, Time, Dose, Ligand]
     cellperexp = 200
     zflowTensor, _ = smallDF(cellperexp)
+    rank = 3
 
-    maximizedNK, maximizedFactors, _, _, _ = minimize_func(zflowTensor, rank=3, n_cluster=6)
+    maximizedNK, maximizedFactors, optPTfactors, _, _ = minimize_func(zflowTensor, rank=3, n_cluster=6)
+    ptMarkerPatterns = optPTfactors[1]
 
-    ax[0].bar(np.arange(1, maximizedNK.size + 1), maximizedNK)
+    for i in range(rank):
+        dff = pd.DataFrame(ptMarkerPatterns[:, :, i], columns=markerslist, index=markerslist)
+        sns.heatmap(data=dff, ax=ax[i])
+
+    ax[3].bar(np.arange(1, maximizedNK.size + 1), maximizedNK)
     xlabel = "Cluster"
     ylabel = "NK Value"
-    ax[0].set(xlabel=xlabel, ylabel=ylabel)
+    ax[3].set(xlabel=xlabel, ylabel=ylabel)
 
     for i in range(0, len(maximizedFactors)):
-        sns.heatmap(data=maximizedFactors[i], vmin=0, ax=ax[i + 1])
+        sns.heatmap(data=maximizedFactors[i], vmin=0, ax=ax[i + 4])
 
     return f
