Fixed the covariance handling! (#77)
* Fixed the covariance handling!

* Reduce the dataset size

gmm/figures/figure6.py
@@ -12,21 +12,19 @@ def makeFigure():
 
     # Add subplot labels
     subplotLabel(ax)
-    blob_DF = make_synth_pic(magnitude=80)
-    plot_synth_pic(blob_DF, t=0, ax=ax[0])
-    plot_synth_pic(blob_DF, t=6, ax=ax[1])
-    plot_synth_pic(blob_DF, t=12, ax=ax[2])
-    plot_synth_pic(blob_DF, t=19, ax=ax[3])
+    blob_DF = make_synth_pic(magnitude=60)
+
+    for i in np.arange(0, 4):
+        plot_synth_pic(blob_DF, t=i * 3, ax=ax[i])
 
     rank = 6
     n_cluster = 6
     blob_xarray = make_blob_tensor(blob_DF)
 
-    maximizedNK, optCP, optPTfactors, _, _, preNormOptCP = minimize_func(blob_xarray, rank=rank, n_cluster=n_cluster, maxiter=1000)
+    maximizedNK, optCP, optPTfactors, _, _, preNormOptCP = minimize_func(blob_xarray, rank=rank, n_cluster=n_cluster, maxiter=2000)
 
     for i in np.arange(0, 4):
-        print(i)
-        points = gen_points_GMM(maximizedNK, preNormOptCP, optPTfactors, i * 6, 0, 0)
+        points = gen_points_GMM(maximizedNK, preNormOptCP, optPTfactors, i * 3, 0, 0)
         points_DF = pd.DataFrame({"Cluster": points[1], "X": points[0][:, 0], "Y": points[0][:, 1]})
         sns.scatterplot(data=points_DF, x="X", y="Y", hue="Cluster", palette="tab10", ax=ax[i + 8])
         ax[i+8].set(xlim=(-.2, 2.2), ylim=(-.2, 2.2))
@@ -63,7 +61,7 @@ palette = {"Ground": "khaki",
 
 def make_synth_pic(magnitude):
     """Makes blob of points depicting beach scene with sinusoidally moving sun"""
-    ts = np.arange(0, 101)
+    ts = np.arange(10)
     blob_DF = None
 
     for t in ts:

gmm/tensor.py
@@ -9,6 +9,7 @@ from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import KFold
 from jax import value_and_grad, jit, grad
 from jax.experimental.host_callback import id_print
+from jax.lax.linalg import triangular_solve
 from scipy.optimize import minimize
 from tensorly.cp_tensor import cp_normalize
 
@@ -94,15 +95,18 @@ def comparingGMMjax(X, nk, meanFact: list, tPrecision):
     return loglik
 
 
-def covFactor_to_precisions(covFac, returnCov=True):
+def covFactor_to_precisions(covFac, returnCov=False):
     """Convert from the cholesky decomposition of the covariance matrix, to the precision matrix."""
-    cholBuilt = jnp.einsum("ax,bcx,dx,ex,fx->abcdef", *covFac)
+    covBuilt = jnp.einsum("ax,bcx,dx,ex,fx->abcdef", *covFac)
+    origShape = covBuilt.shape
     if returnCov:
-        return cholBuilt
-    cholMv = jnp.moveaxis(cholBuilt, (1, 2), (4, 5))
-    cholMv = jnp.linalg.inv(cholMv)
-    precBuild = jnp.moveaxis(cholMv, (4, 5), (1, 2))
-    assert cholBuilt.shape == precBuild.shape
+        return covBuilt
+    covBuilt = jnp.moveaxis(covBuilt, (1, 2), (4, 5))
+    Y = jnp.broadcast_to(jnp.eye(covBuilt.shape[4])[jnp.newaxis, jnp.newaxis, jnp.newaxis, jnp.newaxis, :, :], covBuilt.shape)
+    assert covBuilt.shape == Y.shape
+    precBuild = triangular_solve(covBuilt, Y, lower=True)
+    precBuild = jnp.moveaxis(precBuild, (4, 5), (1, 2))
+    assert origShape == precBuild.shape
     return precBuild
 
 
@@ -190,10 +194,10 @@ def sample_GMM(weights_, means_, cholCovs, n_samples):
 def gen_points_GMM(optNK, optCP, optPT, time, dose, ligand):
     """Generates points from a scikit-learn GMM object for a fit NK, CP and PT"""
     cholCov = covFactor_to_precisions(optPT, returnCov=True)
-    cholCov = np.squeeze(cholCov[:, :, :, time, dose, ligand]) / 100.0
+    cholCov = cholCov[:, :, :, time, dose, ligand]
     means = tl.cp_to_tensor((None, optCP))
 
-    nk = np.array(optNK / np.sum(optNK))
-    means = np.array(means[:, :, time, dose, ligand])
-    samples = sample_GMM(nk, means, np.array(cholCov), 1000)
+    nk = optNK / np.sum(optNK)
+    means = means[:, :, time, dose, ligand]
+    samples = sample_GMM(nk, means, cholCov, 1000)
     return samples