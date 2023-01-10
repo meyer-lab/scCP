7c89fb864b552e40978ee9a4a5842a096107e212
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-01-10 09:34:38 -0800

    Plotting projections for one specific condition

--- sccp/figures/figure1.py
+++ sccp/figures/figure1.py
@@ -9,7 +9,7 @@ from tensorly.decomposition import parafac2
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((12, 12), (2, 2))
+    ax, f = getSetup((12, 12), (4, 5))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -18,7 +18,7 @@ def makeFigure():
     drugXA = ThompsonXA_SCGenes()
 
     # Performing parafac2 on single-cell Xarray
-    _, factors, _ = parafac2(
+    _, factors, projs = parafac2(
         drugXA.to_numpy(),
         rank=5,
         n_iter_max=10,
@@ -26,6 +26,6 @@ def makeFigure():
         verbose=True,
     )
 
-    plotSCCP_factors(factors, drugXA, ax)
+    plotSCCP_factors(factors, drugXA, projs[0,:,:], ax)
 
     return f
