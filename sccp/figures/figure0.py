a202ba0f09b6ac11928a4b8b66a1efc000f5aae9
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2023-01-12 07:06:33 -0800

    Fixing figure 0 (#9)
    * Fixing figure 0
    
    * Black formatting
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    Co-authored-by: Aaron Meyer <ameyer@ucla.edu>

---
+++ sccp/figures/figure0.py
@@ -0,0 +1,36 @@
+"""
+Creating synthetic data and implementation of parafac2
+"""
+import numpy as np
+from .common import subplotLabel, getSetup, plotSCCP_factors
+from ..synthetic import synthXA, plot_synth_pic
+from tensorly.decomposition import parafac2
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((12, 12), (4, 4))
+
+    # Add subplot labels
+    subplotLabel(ax)
+
+    blobXA, blobDF = synthXA(magnitude=100, type="beach")
+
+    weights, factors, projs = parafac2(
+        blobXA.to_numpy(),
+        rank=3,
+        n_iter_max=1,
+        normalize_factors=True,
+        verbose=True,
+    )
+
+    plotSCCP_factors(factors, blobXA, projs[0], ax)
+
+    for i in np.arange(0, 3):
+        plot_synth_pic(blobDF, t=i * 3, palette=palette, ax=ax[i + 12])
+
+    return f
+
+
+palette = {"Ground": "khaki", "Trunk": "sienna", "Leaf": "limegreen", "Sun": "yellow"}
