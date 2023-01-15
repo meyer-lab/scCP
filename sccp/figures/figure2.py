1eb16d97f5d08a73b21a870110fd0a96b5443e57
Aaron Meyer <ameyer@ucla.edu>
2023-01-15 09:22:25 -0800

    Fix function calls

--- sccp/figures/figure2.py
+++ sccp/figures/figure2.py
@@ -3,7 +3,7 @@ Parafac2 implementation on PBMCs treated across IL2 treatments, times, and doses
 """
 from .common import subplotLabel, getSetup, plotSCCP_factors
 from ..imports.cytok import IL2_flowXA
-from ..parafac2 import parafac2
+from ..parafac2 import parafac2_nd
 
 
 def makeFigure():
@@ -21,7 +21,7 @@ def makeFigure():
     flowXA = flowXA.loc[:, :, :, :2000, :]
 
     # Performing parafac2 on single-cell Xarray
-    _, factors, projs = parafac2(
+    _, factors, projs = parafac2_nd(
         flowXA.to_numpy(), rank=3, nn_modes=(0, 1, 2), verbose=True
     )
 
