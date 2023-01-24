4ab606376f7d74560a3a75439228747abf7ca343
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-01-24 10:31:18 -0800

    Updating R2x plot only use one parafac2 and adding synthetic option

--- sccp/tensor.py
+++ sccp/tensor.py
@@ -1,12 +1,9 @@
 import os
 from os.path import join
 import numpy as np
-from .parafac2 import _parafac2_rec_error, parafac2 as pf2
-from tensorly.decomposition import parafac2
-
+from .parafac2 import _parafac2_rec_error, parafac2
 
 path_here = os.path.dirname(os.path.dirname(__file__))
-
 
 def plotR2X(tensor, rank, datatype, ax, runPf2=False):
     """Creates R2X plot for parafac2 tensor decomposition"""
@@ -14,19 +11,7 @@ def plotR2X(tensor, rank, datatype, ax, runPf2=False):
         rank_vec = np.arange(1, rank + 1)
         pf2_error = np.empty(len(rank_vec))
         for i in range(len(rank_vec)):
-            if len(tensor.shape) == 3:
-                weights, factors, projs = parafac2(
-                    tensor.to_numpy(),
-                    rank=rank,
-                    n_iter_max=10,
-                    normalize_factors=True,
-                    verbose=True,
-                )
-
-                rearrange = False
-
-            else:
-                weights, factors, projs = pf2(
+            weights, factors, projs = parafac2(
                     tensor.to_numpy(),
                     rank=rank,
                     n_iter_max=10,
@@ -34,7 +19,11 @@ def plotR2X(tensor, rank, datatype, ax, runPf2=False):
                     verbose=True,
                 )
 
+            if len(tensor.shape) == 3:
+                rearrange = False
+            else:
                 rearrange = True
+            
 
             pf2_error[i] = _parafac2_rec_error(
                 tensor.to_numpy(), [weights, factors, projs], rearrangeProjs=rearrange
@@ -46,6 +35,8 @@ def plotR2X(tensor, rank, datatype, ax, runPf2=False):
             np.save(join(path_here, "sccp/data/CoH_Pf2_Errors.npy"), pf2_error)
         elif datatype == "scRNA":
             np.save(join(path_here, "sccp/data/scRNA_Pf2_Errors.npy"), pf2_error)
+        elif datatype == "Synthetic":
+            np.save(join(path_here, "sccp/data/Synthetic_Pf2_Errors.npy"), pf2_error)
 
     else:
         if datatype == "IL2":
@@ -59,6 +50,10 @@ def plotR2X(tensor, rank, datatype, ax, runPf2=False):
         elif datatype == "scRNA":
             pf2_error = np.load(
                 join(path_here, "sccp/data/scRNA_Pf2_Errors.npy"), allow_pickle=True
+            )
+        elif datatype == "Synthetic":
+            pf2_error = np.load(
+                join(path_here, "sccp/data/Synthetic_Pf2_Errors.npy"), allow_pickle=True
             )
 
         rank_vec = np.arange(1, len(pf2_error) + 1)
