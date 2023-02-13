b91bdd07548ad0407e10aebfd599ff096fb441cf
Aaron Meyer <ameyer@ucla.edu>
2023-02-13 07:21:51 -0800

    Move normalization to the end of solving

--- sccp/parafac2.py
+++ sccp/parafac2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tensorly.tenalg import khatri_rao
 from opt_einsum import contract
-from tensorly.cp_tensor import cp_flip_sign
+from tensorly.cp_tensor import cp_flip_sign, cp_normalize
 from tensorly.decomposition import non_negative_parafac_hals
 from tensorly.decomposition._parafac2 import (
     _project_tensor_slices,
@@ -12,10 +12,10 @@ import tlviz
 
 
 def _parafac2_reconstruction_error(X, decomposition):
-    weights, (A, B, C), projections = decomposition
+    (A, B, C), projections = decomposition
     proj = np.stack(projections, axis=0)
 
-    reconstruction = contract("k,ijm,mk,ik,lk->ijl", weights, proj, B, A, C)
+    reconstruction = contract("ijm,mk,ik,lk->ijl", proj, B, A, C)
     return np.linalg.norm(X - reconstruction)
 
 
@@ -35,7 +35,7 @@ def parafac2_nd(
     # *** THIS IMPLEMENTATION REQUIRES A SINGLE ZERO-PADDED TENSOR. ***
     # Unless otherwise labelled, variables are structured as 3D.
     X = np.reshape(X_nd, (-1, X_nd.shape[-2], X_nd.shape[-1]))
-    weights, factors, projections = initialize_decomposition(
+    _, factors, projections = initialize_decomposition(
         X, rank, init=init, svd=svd, random_state=random_state
     )
 
@@ -43,9 +43,6 @@ def parafac2_nd(
     norm_tensor = np.linalg.norm(X) ** 2
 
     for iter in range(n_iter_max):
-        factors[1] = factors[1] * np.reshape(weights, (1, -1))
-        weights = np.ones(weights.shape)
-
         projections = _compute_projections(X, factors, svd)
         projected_tensor = _project_tensor_slices(X, projections)
 
@@ -55,22 +52,20 @@ def parafac2_nd(
             projected_tensor, (*X_nd.shape[0:-2], rank, X_nd.shape[-1])
         )
 
-        weights, factors_nD = non_negative_parafac_hals(
+        _, factors_nD = non_negative_parafac_hals(
             projected_tensor_nD,
             rank,
             n_iter_max=n_iter_parafac,
-            init=init if iter == 0 else (weights, factors_nD),
+            init=init if iter == 0 else (None, factors_nD),
             svd=svd,
             nn_modes=nn_modes,
-            normalize_factors=iter > 0,
             tol=False,
         )
-        weights, factors_nD = cp_flip_sign((weights, factors_nD), mode=1)
 
         # Convert factors to 3D
         factors = [khatri_rao(factors_nD[:-2]), factors_nD[-2], factors_nD[-1]]
 
-        rec_error = _parafac2_reconstruction_error(X, (weights, factors, projections))
+        rec_error = _parafac2_reconstruction_error(X, (factors, projections))
 
         errs.append(rec_error / norm_tensor)
 
@@ -87,6 +82,9 @@ def parafac2_nd(
         else:
             if verbose:
                 print(f"iteration 1: error={errs[-1]}.")
+
+    weights, factors_nD = cp_normalize((None, factors_nD))
+    weights, factors_nD = cp_flip_sign((weights, factors_nD), mode=1)
 
     consistency = tlviz.model_evaluation.core_consistency((weights, factors_nD), projected_tensor_nD, normalised=True)
     print(f"Core consistency = {consistency}.")
