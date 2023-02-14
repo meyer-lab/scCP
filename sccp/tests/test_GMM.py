99222d008617cd60ee9369cb6bb2c8e8b1053843
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-02-14 08:49:51 -0800

    Remove test

--- sccp/tests/test_GMM.py
+++ sccp/tests/test_GMM.py
@@ -4,15 +4,8 @@ Test the data import.
 import pytest
 import numpy as np
 from ..imports.cytok import IL2_flowXA
-from ..imports.CoH import CoH_xarray
 
 IL2data_import, _ = IL2_flowXA()
-
-def test_import_CoH():
-    """Test the CoH import."""
-    cohXA, celltypeXA = CoH_xarray(saveXA=False)
-    assert np.isfinite(cohXA[:, :, :, :10].to_numpy()).all()
-
 
 def test_finite_data():
     """Test that all values in tensor has no NaN"""
