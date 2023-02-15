d3d7552db01251f407a1c2388423d2c4ceb55080
Aaron Meyer <ameyer@ucla.edu>
2023-02-15 12:15:11 -0800

    Restart build

--- sccp/parafac.py
+++ sccp/parafac.py
@@ -4,8 +4,7 @@ from tensorly.decomposition._cp import initialize_cp
 
 
 def unfolding_dot_khatri_rao(tensor, factors, mode):
-    """mode-n unfolding times khatri-rao product of factors
-    """
+    """mode-n unfolding times khatri-rao product of factors"""
     tensor_idx = "".join(chr(ord("a") + i) for i in range(tensor.ndim))
     rank = chr(ord("a") + tensor.ndim + 1)
     op = tensor_idx
@@ -27,15 +26,10 @@ def parafac(
     svd="truncated_svd",
 ):
     """A simple CP ALS."""
-    _, factors = initialize_cp(
-        tensor,
-        rank,
-        init=init,
-        svd=svd,
-    )
+    _, factors = initialize_cp(tensor, rank, init=init, svd=svd)
     for _ in range(n_iter_max):
         for mode in range(tensor.ndim):
-            other_factors = [factors[i] for i in range(len(factors)) if i != mode]
+            other_factors = [f for (i, f) in enumerate(factors) if i != mode]
 
             pinv = np.ones((rank, rank))
             for f in other_factors:
