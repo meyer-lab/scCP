169fbe3a892557af55eecc5a5b4141983fda1d57
Aaron Meyer <ameyer@ucla.edu>
2023-02-18 14:23:28 -0800

    Might be done

--- sccp/parafac2.py
+++ sccp/parafac2.py
@@ -1,71 +1,73 @@
 import numpy as np
-from tensorly.tenalg import khatri_rao
-from opt_einsum import contract
 from tensorly.cp_tensor import cp_flip_sign, cp_normalize
-from .parafac import parafac
-from tensorly.decomposition._parafac2 import (
-    _project_tensor_slices,
-    _compute_projections,
-    initialize_decomposition,
-)
+import tensorly as tl
+from tensorly.decomposition import parafac
 import tlviz
 
 
-def _parafac2_reconstruction_error(X, decomposition):
-    (A, B, C), projections = decomposition
-    proj = np.stack(projections, axis=0)
+def _compute_projections(X_nd, recon_nD, rank):
+    X = np.reshape(X_nd, (-1, X_nd.shape[-2], X_nd.shape[-1]))
+    recon = np.reshape(recon_nD, (-1, rank, X_nd.shape[-1]))
 
-    reconstruction = contract("ijm,mk,ik,lk->ijl", proj, B, A, C)
-    return np.linalg.norm(X - reconstruction)
+    projections = np.empty((X.shape[0], X.shape[1], rank))
+    proj_X = np.empty((X.shape[0], rank, X.shape[2]))
+    error = 0.0
+
+    for i in range(X.shape[0]):
+        U, _, Vh = tl.tenalg.svd.truncated_svd(recon[i] @ X[i].T, rank)
+        projections[i] = (U @ Vh).T
+        error += np.linalg.norm(X[i] - projections[i] @ recon[i]) ** 2
+        proj_X[i] = projections[i].T @ X[i]
+
+    # Convert projections and projected tensor to nD
+    projections_nD = np.reshape(projections, (*X_nd.shape[0:-1], rank))
+    projected_X_nD = np.reshape(
+        proj_X, (*X_nd.shape[0:-2], rank, X_nd.shape[-1])
+    )
+
+    return projections_nD, projected_X_nD, error
 
 
 def parafac2_nd(
     X_nd,
     rank,
     n_iter_max=200,
-    init="svd",
-    svd="randomized_svd",
-    tol=1e-12,
-    random_state=None,
+    tol=1e-5,
     verbose=False,
-    n_iter_parafac=200,
 ):
     r"""The same interface as regular PARAFAC2."""
     # *** THIS IMPLEMENTATION REQUIRES A SINGLE ZERO-PADDED TENSOR. ***
-    # Unless otherwise labelled, variables are structured as 3D.
-    X = np.reshape(X_nd, (-1, X_nd.shape[-2], X_nd.shape[-1]))
-    _, factors, projections = initialize_decomposition(
-        X, rank, init=init, svd=svd, random_state=random_state
-    )
+
+    # Initialization
+    unfolded_mode_2 = tl.unfold(X_nd, X_nd.ndim - 1)
+    assert rank < np.shape(unfolded_mode_2)[0]
+    S, _, D = tl.svd_interface(unfolded_mode_2, n_eigenvecs=rank, method="randomized_svd")
+    projections_nD = np.reshape(D, (*X_nd.shape[0:-1], rank))
+    projected_tensor_nD = np.einsum("...jk,...jl->...kl", projections_nD, X_nd)
+
+    factors_nD = [np.ones((X_nd.shape[i], rank)) for i in range(X_nd.ndim - 2)]
+    factors_nD += [np.eye(rank), S]
+    # End initialization
 
     errs = []
-    norm_tensor = np.linalg.norm(X) ** 2
+    norm_tensor = np.linalg.norm(X_nd) ** 2
 
     for iter in range(n_iter_max):
-        projections = _compute_projections(X, factors, svd)
-        projected_tensor = _project_tensor_slices(X, projections)
-
-        # Convert projections and projected tensor to nD
-        projections_nD = np.reshape(projections, (*X_nd.shape[0:-1], rank))
-        projected_tensor_nD = np.reshape(
-            projected_tensor, (*X_nd.shape[0:-2], rank, X_nd.shape[-1])
-        )
-
-        factors_nD = parafac(
+        _, factors_nD = parafac(
             projected_tensor_nD,
             rank,
-            n_iter_max=n_iter_parafac,
-            init=init if iter == 0 else (None, factors_nD),
+            init=(None, factors_nD),
+            n_iter_max=4,
+            svd="no svd", # should never be used anyway
+            tol=None,
         )
 
-        # Convert factors to 3D
-        factors = [khatri_rao(factors_nD[:-2]), factors_nD[-2], factors_nD[-1]]
+        reconstruction_nD = tl.cp_to_tensor((None, factors_nD))
 
-        rec_error = _parafac2_reconstruction_error(X, (factors, projections)) ** 2
-
+        projections_nD, projected_tensor_nD, rec_error = _compute_projections(X_nd, reconstruction_nD, rank)
         errs.append(rec_error / norm_tensor)
 
-        if iter >= 1:
+        if iter > 0:
             if verbose:
                 print(
                     f"iteration {iter + 1}: error={errs[-1]}, Δ={errs[-2] - errs[-1]}."
@@ -75,9 +77,6 @@ def parafac2_nd(
                 if verbose:
                     print(f"converged in {iter + 1} iterations.")
                 break
-        else:
-            if verbose:
-                print(f"iteration 1: error={errs[-1]}.")
     
     weights, factors_nD = cp_normalize((None, factors_nD))
     weights, factors_nD = cp_flip_sign((weights, factors_nD), mode=1)
