a91eddbd16ce52f23c3c9c3cf309d059ee920b56
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-03-14 12:47:42 -0700

    Gini coeff successful

--- sccp/figures/figure0.py
+++ sccp/figures/figure0.py
@@ -64,26 +64,6 @@ def makeFigure():
     # plotR2X(blobInfo["data"].to_numpy(), 3, ax[7])
     # plotCrossVal(blobInfo["data"].to_numpy(), 3,  ax[8], trainPerc=0.75)
     
-    pj = flattened_projs["projections"].to_numpy()
-    array = np.sort(pj[1, :])
-    if np.amin(array) < 0:
-        array -= np.amin(array)
-    index = np.arange(1,array.shape[0]+1)
-    n = array.shape[0]
-    gini = ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array))) 
-    
-    # print(gini)
-    # array = array.flatten() #all values are treated equally, arrays must be 1d
-    # if np.amin(array) < 0:
-    #     array -= np.amin(array) #values cannot be negative
-    # array += 0.0000001 #values cannot be 0
-    # array = np.sort(array) #values must be sorted
-    # index = np.arange(1,array.shape[0]+1) #index per array element
-    # n = array.shape[0]#number of array elements
-    # return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array))) 
-    
-    
-  
     return f
 
 def gini_coeff(projs: xa.Dataset):
@@ -92,20 +72,14 @@ def gini_coeff(projs: xa.Dataset):
     
     gini = np.empty(proj_data.shape[0])
     for i in range(proj_data.shape[0]):
-        print(i)
         projComp = np.sort(proj_data[i, :])
         if np.amin(projComp) < 0:
             projComp -= np.amin(projComp)
+            
         index = np.arange(1, projComp.shape[0]+1)
-    
-        gini[i] = ((np.sum((2 * index - projComp.shape[0]  - 1) * projComp)) / 
-                projComp.shape[0] * np.sum(projComp))
-        
-    idx_gini = np.argsort(gini)
-    
-    print(idx_gini)
+        gini[i] = (np.sum((2 * index - projComp.shape[0]  - 1) * projComp)) / (projComp.shape[0] * np.sum(projComp))
+   
     print(gini)
-        
     return
         
         
