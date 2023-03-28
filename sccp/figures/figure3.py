d5f1f49d8924be2ce94ea4d9e9638941d10763e0
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-03-28 13:44:12 -0700

    Raw cell type count for each drug

--- sccp/figures/figure3.py
+++ sccp/figures/figure3.py
@@ -2,26 +2,20 @@
 Parafac2 implementation on PBMCs treated wtih PopAlign/Thompson drugs
 """
 import numpy as np
-import xarray as xa
 import pandas as pd
 import seaborn as sns
 from .common import (
     subplotLabel,
     getSetup,
-    plotFactors,
-    plotProj,
-    plotSS,
 )
 from ..imports.scRNA import ThompsonXA_SCGenes
-from ..parafac2 import parafac2_nd
-from ..decomposition import plotR2X
-from ..crossVal import plotCrossVal
+
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((12, 12), (3, 3))
+    ax, f = getSetup((12, 20), (4, 2))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -29,46 +23,15 @@ def makeFigure():
     # Import of single cells: [Drug, Cell, Gene]
     data = ThompsonXA_SCGenes(saveXA=False, offset=1.0)
     
-    drugXA = data.sel(Drug=["Budesonide", "CTRL2"], Gene= ["CD163", "ADORA3", "MS4A6A"])
-    df = drugXA.to_dataframe().reset_index()
+    celltype = data["Cell Type"]
+    df = celltype.to_dataframe().reset_index()
+    df = df.groupby(["Drug", "Cell Type"]).size().reset_index()
+    cl = df.columns
+    df.columns = [cl[0], cl[1], "Count"]
     
-    sns.violinplot(data=df.loc[df["Gene"] == "ADORA3"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1,split=True, ax=ax[0])
-    
-    sns.violinplot(data=df.loc[df["Gene"] == "CD163"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1,split=True, ax=ax[1])
-    
-    sns.violinplot(data=df.loc[df["Gene"] == "MS4A6A"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1, split=True, ax=ax[2])
-    
-    drugXA = data.sel(Drug=["Dexrazoxane HCl (ICRF-187, ADR-529)", "CTRL6"], Gene= ["CD163", "ADORA3", "MS4A6A"])
-    df = drugXA.to_dataframe().reset_index()
-    
-    sns.violinplot(data=df.loc[df["Gene"] == "ADORA3"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1,split=True, ax=ax[3])
-    
-    sns.violinplot(data=df.loc[df["Gene"] == "CD163"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1,split=True, ax=ax[4])
-
-    sns.violinplot(data=df.loc[df["Gene"] == "MS4A6A"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1, split=True, ax=ax[5])
-    
-    drugXA = data.sel(Drug=["Flurbiprofen", "CTRL6"], Gene= ["LTA", "IL2RA", "CD40LG"])
-    df = drugXA.to_dataframe().reset_index()
-    
-    sns.violinplot(data=df.loc[df["Gene"] == "LTA"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1,split=True, ax=ax[6])
-    
-    sns.violinplot(data=df.loc[df["Gene"] == "IL2RA"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1,split=True, ax=ax[7])
-
-    sns.violinplot(data=df.loc[df["Gene"] == "CD40LG"] , x="Cell Type", y="data", hue="Drug",
-               linewidth=1, split=True, ax=ax[8])
-    
-    
-    genes = ["CD163", "ADORA3", "MS4A6A", "CD163", "ADORA3", "MS4A6A", "LTA", "IL2RA", "CD40LG"]
-    for i in range(9):
-        ax[i].tick_params(axis="x", rotation=45)
-        ax[i].set_title("Gene:"+ genes[i])
+    for i, cell in enumerate(np.unique(celltype)):
+        sns.barplot(data=df.loc[df["Cell Type"] == cell], x = "Drug", y = "Count", ax=ax[i])
+        ax[i].set_title(cell)
+        ax[i].tick_params(axis="x", rotation=90)
     
     return f
