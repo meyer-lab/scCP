f6b33d568729fc81f31dcaf4e20f49ac40b13e1a
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-04-05 20:57:01 -0700

    Framework for UMAP

--- sccp/figures/figure5.py
+++ sccp/figures/figure5.py
@@ -44,104 +44,8 @@ def makeFigure():
 
     # Find cells associated with drugs
     drugs = ["Triamcinolone Acetonide", "Budesonide", "Betamethasone Valerate", "Dexrazoxane HCl (ICRF-187, ADR-529)"]
-    plotDrugUMAP(drugs, data.condition_labels, umapPoints, ax[5:9])
- 
-    
-   
-    
-    # druglist[druglist == True] = 1
-    # druglist[druglist == False] = 0
-    # print(druglist)
-    # print(np.shape(druglist))
-    # for i, drugz in enumerate(drugs):
-    #     flatData = flat_data.where(flat_data.Drug == drugz).to_numpy()
-    #     flatData = np.nan_to_num(flatData[0, :], nan=-1)
-    #     flatData[flatData != -1] = 1
+    plotDrugUMAP(drugs, dataDF["Drug"].values, umapPoints, ax[5:9])
 
-    #     # Creating DF for figure
-    #     umapDF = pd.DataFrame({"UMAP1": umapPoint s[:, 0],
-    #             "UMAP2": umapPoints[:, 1],
-    #             drugz: flatData,
-    #         })
-    #     sns.scatterplot(data=umapDF, x="UMAP1", y="UMAP2",hue=drugz, s=0.5, ax=ax[i])
-        
-    # print(np.shape(drugs))  
-    # print(np.shape(totalProjs))
-    
-    # print(np.hstack((totalProjs,np.reshape(drugs, [-1, 1]))))
-        
-    
-    
-    
-    # DF = pd.DataFrame([])
-    # for i in range(factors[0].shape[0]): # for each drug
-        
-    #     df = pd.DateFrame([data=projs[i], columns=)
-    #     DF = pd.concat([DF, df],axis=1)
-        
-    # data.condition_labels
-    #     projs
-    # #     totalProjs[i, :] = projs[0]
-    # flatProjs = np.concatenate(projs, axis=0)
-
-    # # idxx = np.random.choice(flatProjs.shape[0], size=200, replace=False)
-    # # flatProjs = flatProjs[idxx, :]
-    # proj = np.array(projs)
-    # print(np.array(projs))
-    # print(projs.to_numpy())
-    # print(np.shape(proj))
-    # projs = xa.DataArray(
-    #     projs,
-    #     dims=["Drug", "Cell", "Cmp"],
-    #     coords=dict(
-    #         Drug=data.coords["Drug"],
-    #         Cell=data.coords["Cell"],
-    #         Cmp=[f"Cmp. {i}" for i in np.arange(1, projs.shape[2] + 1)],
-    #     ),
-    
-    #     name="projections",
-    # )
-    # projs = xa.merge([projs, data["Cell Type"]], compat="no_conflicts")
-
-    # flattened_projs = projs.stack(AllCells=("Drug", "Cell"))
-    # flat_data = data["data"].stack(AllCells=(("Drug", "Cell")))
-  
-    # # Remove empty slots
-    # nonzero_index = np.any(flattened_projs["projections"].to_numpy() != 0, axis=0)
-    # flattened_projs = flattened_projs.isel(AllCells=nonzero_index) 
-    # flat_data = flat_data.isel(AllCells=nonzero_index) 
-    
-    # # UMAP dimension reduction
-    # umapReduc = umap.UMAP()
-    # umapPoints = umapReduc.fit_transform(flattened_projs["projections"].to_numpy().T)
-    
-    # # Find cells associated with drugs
-    # drugs = ["Triamcinolone Acetonide", "Budesonide", "Betamethosone Valerate", "Dexrazoxane HCl (ICRF-187, ADR-529)"]
-    
-    # for i, drugz in enumerate(drugs):
-    #     flatData = flat_data.where(flat_data.Drug == drugz).to_numpy()
-    #     flatData = np.nan_to_num(flatData[0, :], nan=-1)
-    #     flatData[flatData != -1] = 1
-
-    #     # Creating DF for figure
-    #     umapDF = pd.DataFrame({"UMAP1": umapPoints[:, 0],
-    #             "UMAP2": umapPoints[:, 1],
-    #             drugz: flatData,
-    #         })
-    # #     sns.scatterplot(data=umapDF, x="UMAP1", y="UMAP2",hue=drugz, s=0.5, ax=ax[i])
-        
-    
-    # genes = ["LAD1", "NKG7", "CD8A", "CD33", "CCR7"]
-    # # DC, NK, CD8, Mono, CD4]
-    # for i, genez in enumerate(genes):
-    #     gene_values = flat_data.sel(Gene=genez).to_numpy()
-    #     tl = ax[i+4].scatter(umapPoints[::25, 0], umapPoints[::25, 1], c=gene_values[::25], cmap ="cool", s=0.5)
-    #     f.colorbar(tl, ax=ax[i+4])
-    #     ax[i+4].set_xlabel("UMAP1")
-    #     ax[i+4].set_ylabel("UMAP2")
-    #     ax[i+4].set_title(genez)
-
-    
     return f
 
 def flattenData(data, factors, projs):
@@ -187,5 +91,7 @@ def plotDrugUMAP(drugs, totaldrugs, umapPoints, axs):
                 drugz: drugList[::100],
             })
         sns.scatterplot(data=umapDF, x="UMAP1", y="UMAP2", hue=drugz, s=5, ax=axs[i])
+        
+    return
     
  
