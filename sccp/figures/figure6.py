c10722e2ebe3cd886c88e333397a68a38d5129bb
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-04-07 15:19:34 -0700

    Perturb Names

--- sccp/figures/figure6.py
+++ sccp/figures/figure6.py
@@ -12,6 +12,7 @@ from ..imports.scRNA import import_perturb_RPE
 from ..parafac2 import parafac2_nd
 from ..decomposition import plotR2X
 import seaborn as sns
+import mygene
 
 
 def makeFigure():
@@ -29,6 +30,14 @@ def makeFigure():
     delidx = np.array([xx.shape[0] > 200 for xx in X.X_list], dtype=bool)
     X.X_list = [X.X_list[ii] for ii in range(delidx.size) if delidx[ii]]
     X.condition_labels = X.condition_labels[delidx]
+    X.condition_labels = np.array([X.condition_labels[ii].split("_")[0] for ii in range(X.condition_labels.size)])
+
+    mg = mygene.MyGeneInfo()
+    ginfo = mg.querymany(X.variable_labels, scopes='ensembl.gene')
+    for ii in range(X.variable_labels.size):
+        if "symbol" in ginfo[ii]:
+            X.variable_labels[ii] = ginfo[ii]["symbol"]
+
 
     # Performing parafac2 on single-cell Xarray
     _, factors, projs, _ = parafac2_nd(
@@ -36,6 +45,7 @@ def makeFigure():
         rank=24,
         verbose=True,
     )
+
 
     plotFactors(factors, X, ax[0:2], reorder=(0, 2), trim=(2,))
 
@@ -46,6 +56,6 @@ def makeFigure():
         cmap=sns.diverging_palette(240, 10, as_cmap=True),
     )
 
-    plotR2X(X, 6, ax[3])
+    #plotR2X(X, 24, ax[3])
 
     return f
