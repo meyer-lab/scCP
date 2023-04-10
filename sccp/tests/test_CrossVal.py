0f74d4dfd5d9456494b9bbc102e75e3c5ebdd63c
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2023-04-10 13:27:55 -0700

    Reproducible factorization (#79)
    * Reproducible factorization
    
    * Try this.

--- sccp/tests/test_CrossVal.py
+++ sccp/tests/test_CrossVal.py
@@ -1,7 +1,6 @@
 """
 Test the cross validation accuracy.
 """
-import tensorly as tl
 import numpy as np
 from tensorly.random import random_parafac2
 from ..crossVal import crossvalidate, crossvalidate_PCA
@@ -10,7 +9,7 @@ from ..crossVal import crossvalidate, crossvalidate_PCA
 def test_crossval():
     """Test for correctness of cross validation."""
     rank = 3
-    X = random_parafac2([(50, 50)] * 8, rank=rank, full=True)
+    X = random_parafac2([(100, 100)] * 8, rank=rank, full=True)
 
     pca_err = crossvalidate_PCA(np.concatenate(X, axis=0), rank=rank, trainPerc=0.8)
     assert pca_err > 0.95
