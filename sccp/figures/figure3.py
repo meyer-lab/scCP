03bab479faafce93161d9a7874dd6f8ebfb3b469
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-04-10 18:03:52 -0700

    Figure size

--- sccp/figures/figure3.py
+++ sccp/figures/figure3.py
@@ -18,7 +18,7 @@ from sklearn.decomposition import PCA
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((6, 6), (2, 2))
+    ax, f = getSetup((8, 10), (4, 3))
 
     # Add subplot labels
     subplotLabel(ax)
