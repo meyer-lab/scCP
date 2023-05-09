Make testing reproducible. Fix sign application issue.

sccp/crossVal.py
@@ -5,13 +5,15 @@ from .parafac2 import parafac2_nd, _cmf_reconstruction_error
 from tensorly.parafac2_tensor import parafac2_to_slices
 
 
-def crossvalidate_PCA(X: np.ndarray, rank: int, trainPerc: float = 0.75) -> np.ndarray:
+def crossvalidate_PCA(X: np.ndarray, rank: int, trainPerc: float = 0.75, random_state=None) -> np.ndarray:
     """Bi-cross-validation for PCA. Because PCA is component-by-component, this
     provides R2X for all ranks in one pass."""
+    rng = np.random.default_rng(random_state)
+
     # Shuffle so that we take a different subset each time
     X = X.copy()
-    X = X[np.random.permutation(X.shape[0]), :]
-    X = X[:, np.random.permutation(X.shape[1])]
+    X = X[rng.permutation(X.shape[0]), :]
+    X = X[:, rng.permutation(X.shape[1])]
 
     # Indices where we will split
     X_B_idx = int(X.shape[0] * trainPerc)
@@ -38,12 +40,14 @@ def crossvalidate_PCA(X: np.ndarray, rank: int, trainPerc: float = 0.75) -> np.n
     return 1.0 - recon_error / total_var
 
 
-def crossvalidate(X, rank: int, trainPerc: float = 0.75, verbose: bool = True) -> float:
+def crossvalidate(X, rank: int, trainPerc: float = 0.75, verbose: bool = False, random_state=None) -> float:
+    rng = np.random.default_rng(random_state)
+
     # Shuffle, rnd.shuffle handles the cell axis
-    var_idx = np.random.permutation(X[0].shape[1])
+    var_idx = rng.permutation(X[0].shape[1])
     X = [xx[:, var_idx] for xx in X]
     for xx in X:
-        np.random.shuffle(xx)
+        rng.shuffle(xx)
 
     X_B_idx = [int(xx.shape[0] * trainPerc) for xx in X]
     B_train = [X[ii][:bi, :] for ii, bi in enumerate(X_B_idx)]

sccp/parafac2.py
@@ -127,7 +127,7 @@ def parafac2_nd(
 
     # Flip the sign based on B
     signn = np.sign(np.diag(CP.factors[1]))
-    CP.factors[1] *= signn[np.newaxis, :]
+    CP.factors[1] *= signn[:, np.newaxis]
     projections = [p * signn for p in projections]
 
     return CP.weights, CP.factors, projections, R2X

sccp/tests/test_CP.py
@@ -8,7 +8,7 @@ from ..parafac2 import parafac2_nd, _cmf_reconstruction_error
 from tensorly.decomposition._parafac2 import _parafac2_reconstruction_error
 
 
-pf2shape = [(100, 8000)] * 20
+pf2shape = [(100, 800)] * 4
 X = random_parafac2(pf2shape, rank=3, full=True, random_state=2)
 norm_tensor = np.linalg.norm(X) ** 2
 
@@ -16,6 +16,11 @@ norm_tensor = np.linalg.norm(X) ** 2
 def test_parafac2():
     """Test for equivalence to TensorLy's PARAFAC2."""
     w1, f1, p1, e1 = parafac2_nd(X, rank=3, random_state=1)
+
+    # Test that the model still matches the data
+    err = _parafac2_reconstruction_error(X, (w1, f1, p1)) ** 2
+    np.testing.assert_allclose(1.0 - err / norm_tensor, e1, rtol=1e-6)
+
     # Test reproducibility
     w2, f2, p2, e2 = parafac2_nd(X, rank=3, random_state=1)
     # Compare to TensorLy
@@ -40,13 +45,10 @@ def test_parafac2():
         np.testing.assert_allclose(p1[ii], p2[ii])
 
     # Compare to TensorLy
-    np.testing.assert_allclose(w1, wT, rtol=0.1)
+    np.testing.assert_allclose(w1, wT, rtol=0.01)
     for ii in range(3):
-        np.testing.assert_allclose(f1[ii], fT[ii], rtol=0.1, atol=0.01)
-        np.testing.assert_allclose(p1[ii], pT[ii], rtol=0.1, atol=0.01)
-
-    err = _parafac2_reconstruction_error(X, (w1, f1, p1)) ** 2
-    np.testing.assert_allclose(1.0 - err / norm_tensor, e1, rtol=1e-6)
+        np.testing.assert_allclose(f1[ii], fT[ii], rtol=0.01, atol=0.01)
+        np.testing.assert_allclose(p1[ii], pT[ii], rtol=0.01, atol=0.01)
 
 
 def test_pf2_r2x():