30a588b3cd3931696ac2c12457961d0311e10811
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-06-15 13:33:01 -0700

    Saving checks to import function

--- sccp/tests/test_anndata.py
+++ sccp/tests/test_anndata.py
@@ -3,67 +3,27 @@ Test that we have correctly setup a Theis lab method function. Functions taken f
 """
 import anndata
 import numpy as np
+from ..parafac2 import parafac2_nd
+from ..imports.anndata import check_adata, check_batch, check_Pf2
+from ..imports.scRNA import tensorFy
+from ..figures.common import flattenData
 
 
 def test_anndata():
-    """Tests import is in correct format"""
+    """Check formatting is correct for aData before and after Pf2"""
     aData = anndata.read_h5ad("/opt/andrew/anndata_test.h5ad")
+    Pf2aData(aData,rank=2, batchName="batch")
 
+
+def Pf2aData(aData, rank, batchName):
+    """Run Pf2 for aData"""
     check_adata(aData)
-    check_batch("batch", aData.obs, verbose=True)
-    split, categories = split_batches(aData.copy(), "batch", return_categories=True)
-
-def check_adata(adata):
-    """Ensures data is AnnData"""
-    if type(adata) is not anndata.AnnData:
-        raise TypeError("Input is not a valid AnnData object")
-
-def check_batch(batch, obs, verbose=False):
-    """Ensures categorica names for batch (i.e. drugs)"""
-    if batch not in obs:
-        raise ValueError(f"column {batch} is not in obs")
-    elif verbose:
-        print(f"Object contains {obs[batch].nunique()} batches.")
-            
-def split_batches(adata, batch, hvg=None, return_categories=False):
-    """Split batches and preserve category information
-
-    :param adata:
-    :param batch: name of column in ``adata.obs``. The data type of the column must be of ``Category``.
-    :param hvg: list of highly variable genes
-    :param return_categories: whether to return the categories object of ``batch``
-    """
-    split = []
-    batch_categories = adata.obs[batch].cat.categories
-    if hvg is not None:
-        adata = adata[:, hvg]
-    for i in batch_categories:
-        split.append(adata[adata.obs[batch] == i].copy())
-    if return_categories:
-        return split, batch_categories
-    return split
-
-
-def merge_adata(*adata_list, **kwargs):
-    """Merge adatas from list while remove duplicated ``obs`` and ``var`` columns
-
-    :param adata_list: ``anndata`` objects to be concatenated
-    :param kwargs: arguments to be passed to ``anndata.AnnData.concatenate``
-    """
-
-    if len(adata_list) == 1:
-        return adata_list[0]
-
-    # Make sure that adatas do not contain duplicate columns
-    for _adata in adata_list:
-        for attr in ("obs", "var"):
-            df = getattr(_adata, attr)
-            dup_mask = df.columns.duplicated()
-            if dup_mask.any():
-                print(
-                    f"Deleting duplicated keys `{list(df.columns[dup_mask].unique())}` from `adata.{attr}`."
-                )
-                setattr(_adata, attr, df.loc[:, ~dup_mask])
-
-    return anndata.AnnData.concatenate(*adata_list, **kwargs)
+    check_batch(batchName, aData.obs, verbose=True)
     
+    X = tensorFy(aData, batchName)
+    rank = 2
+    _, factors, projs, _ = parafac2_nd(X, rank=rank, random_state=1, verbose=True)
+    _, projDF, _ = flattenData(X, factors, projs)
+    aData.obsm["Pf2"] = projDF.values
+    
+    check_Pf2(aData, rank)
