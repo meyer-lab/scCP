0d06b04e1fd392a824c203e066e45f1284cee772
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-06-15 18:00:28 -0700

    separate plotting and function

--- sccp/geneontology.py
+++ sccp/geneontology.py
@@ -2,43 +2,45 @@ import numpy as np
 import gseapy as gp
 import pandas as pd
 import seaborn as sns
+from .figures.common import plotCombGO, plotPvalGO
 
+def geneOntology(cmpNumb: int, geneAmount, goTerms, geneValue):
+    """Plots top Gene Ontology terms for molecular function, 
+    biological process, cellular component. Uses factors as 
+    input for function"""
+    
+    df = pd.read_csv("sccp/data/TopBotGenes_Cmp25.csv").rename(columns={"Unnamed: 0":"Gene"}).set_index("Gene")
+    sort_idx = np.argsort(df.to_numpy(), axis=0)
+    
+    # Specifies enrichment sets to run against
+    geneSets = [
+        "GO_Biological_Process_2021",
+        "GO_Cellular_Component_2021",
+        "GO_Molecular_Function_2021"]
 
-def geneOntology(cmpNumb: int, geneAmount, goTerms, geneValue, axs):
-        """Plots top Gene Ontology terms for molecular function, 
-        biological process, cellular component. Uses factors as 
-        input for function"""
-        
-        df = pd.read_csv("sccp/data/TopBotGenes_Cmp25.csv").rename(columns={"Unnamed: 0":"Gene"}).set_index("Gene")
-        sort_idx = np.argsort(df.to_numpy(), axis=0)
-        
-        # Specifies enrichment sets to run against
-        geneSets = [
-            "GO_Biological_Process_2021",
-            "GO_Cellular_Component_2021",
-            "GO_Molecular_Function_2021"]
-
-        genesTop = np.empty((geneAmount), dtype="<U10")
-        genesBottom = np.empty((geneAmount), dtype="<U10")
-        
-        geneNames = df.index.values[sort_idx[:, cmpNumb-1]]
-        genesTop[:] = np.flip(geneNames[-geneAmount:])  
-        genesBottom[:] = geneNames[:geneAmount]
-        
-        
-        for i in range(len(geneSets)):
-            if geneValue == "Overexpressed":
-                enrichrTopGO = runGO(genesTop, geneSets)
-                CombGO = combinedDF(enrichrTopGO, geneSets[i], goTerms)
-                PvalGO = pvalueDF(enrichrTopGO, geneSets[i], goTerms)
-            else:
+    genesTop = np.empty((geneAmount), dtype="<U10")
+    genesBottom = np.empty((geneAmount), dtype="<U10")
+    
+    geneNames = df.index.values[sort_idx[:, cmpNumb-1]]
+    genesTop[:] = np.flip(geneNames[-geneAmount:])  
+    genesBottom[:] = geneNames[:geneAmount]
+    
+    totalCombDF = pd.DataFrame()
+    totalPvalDF = pd.DataFrame()
+    for i, geneSet in enumerate(geneSets):
+        if geneValue == "Overexpressed":
+            enrichrGO = runGO(genesTop, geneSets)
+        else:
+            enrichrBotGO = runGO(genesBottom, geneSets)
             
-                enrichrBotGO = runGO(genesBottom, geneSets)
-                CombGO = combinedDF(enrichrBotGO, geneSets[i], goTerms)
-                PvalGO = pvalueDF(enrichrBotGO, geneSets[i], goTerms)
-            
-            plotCombGO(CombGO, geneValue, ax=axs[2*i])
-            plotPvalGO(PvalGO, geneValue, ax=axs[(2*i)+1])
+        CombGO = combinedDF(enrichrGO, geneSet, geneValue, goTerms)
+        PvalGO = pvalueDF(enrichrGO, geneSet, geneValue, goTerms)
+        
+        totalCombDF = pd.concat([totalCombDF, CombGO])
+        totalPvalDF = pd.concat([totalPvalDF, PvalGO])
+        
+    return totalCombDF, totalPvalDF
+
             
     
 def runGO(geneList, geneSets):
@@ -53,7 +55,7 @@ def runGO(geneList, geneSets):
     
     return enrichrGO
     
-def combinedDF(enrichrGO, geneSet, goTerms):
+def combinedDF(enrichrGO, geneSet, geneValue, goTerms):
     """Saves combined score for gene terms in DF"""
      # Combined Score
     combined = enrichrGO.loc[
@@ -62,11 +64,11 @@ def combinedDF(enrichrGO, geneSet, goTerms):
             ]
     combined = combined.sort_values(ascending=False)
     combined = combined.iloc[:goTerms]
-    combDF = pd.DataFrame({"Term": combined.index.values, "Combined Score": combined.values})
+    combDF = pd.DataFrame({"Term": combined.index.values, "Combined Score": combined.values, "Expression": geneValue, "Gene Set": geneSet})
     
     return combDF
 
-def pvalueDF(enrichrGO, geneSet, goTerms):
+def pvalueDF(enrichrGO, geneSet, geneValue, goTerms):
     """Saves adjusted p value for gene terms in DF"""
     p_val = enrichrGO.loc[
         enrichrGO["Gene_set"] == geneSet,
@@ -74,25 +76,6 @@ def pvalueDF(enrichrGO, geneSet, goTerms):
             ]
     p_val = p_val.sort_values(ascending=True)
     p_val = p_val.iloc[:goTerms]
-    pvalDF = pd.DataFrame({"Term": p_val.index.values, "Adjusted P-value": p_val.values})
+    pvalDF = pd.DataFrame({"Term": p_val.index.values, "Adjusted P-value": p_val.values, "Expression": geneValue, "Gene Set": geneSet})
     
     return pvalDF
-
-def plotCombGO(GO, geneValue, ax):
-    """Plots combines score for gene ontology"""
-    sns.barplot(
-    data=GO,
-    x="Combined Score",
-    y="Term",
-    ax=ax)
-    ax.set_title(geneValue + "-Genes")
-        
-def plotPvalGO(GO, geneValue, ax):
-    """Plots adjusted p value for gene ontology"""
-    pvalPlot = sns.barplot(
-    data=GO,
-    x="Adjusted P-value",
-    y="Term",
-    ax=ax)
-    ax.set_title(geneValue + "-Genes")
-    pvalPlot.set_xscale("log")
