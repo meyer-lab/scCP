6bcfd17b90dde263ca8313ed7ce33c486fd0c6e3
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2023-06-16 19:51:53 -0700

    Add framework for GO analysis (#108)
    * Add framework for GO analysis
    
    * More information for plots
    
    * Save changes for plots
    
    * SAve changes
    
    * Using Pf2 genes output
    
    * Save changes
    
    * Plotting all scores and pvalues in one function
    
    * Giving gene ontology its own function
    
    * Seperate figures
    
    * Update gene ontology
    
    * Update poetry
    
    * Save figure
    
    * separate plotting and function
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/common.py
+++ sccp/figures/common.py
@@ -340,3 +340,24 @@ def plotCV(data, rank, trainPerc, ax):
     )
 
     ax.legend()
+
+def plotCombGO(GO, geneValue, axs):
+    """Plots combines score for gene ontology"""
+    for i, geneset in enumerate(np.unique(GO["Gene Set"])):
+        pvalPlot = sns.barplot(
+        data=GO.loc[GO["Gene Set"] == geneset],
+        x="Combined Score",
+        y="Term",
+        ax=axs[i])
+        axs[i].set_title(geneValue + "-Genes-" + geneset)
+        
+def plotPvalGO(GO, geneValue, axs):
+    """Plots adjusted p value for gene ontology"""
+    for i, geneset in enumerate(np.unique(GO["Gene Set"])):
+        pvalPlot = sns.barplot(
+        data=GO.loc[GO["Gene Set"] == geneset],
+        x="Adjusted P-value",
+        y="Term",
+        ax=axs[i])
+        axs[i].set_title(geneValue + "-Genes-" + geneset)
+        pvalPlot.set_xscale("log")
