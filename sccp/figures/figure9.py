ad49b7478d161947f4a6a622c15ba0911e6eb439
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-06-28 19:40:13 -0700

    Fixing import name for each import data

--- sccp/figures/figure9.py
+++ sccp/figures/figure9.py
@@ -12,11 +12,11 @@ def makeFigure():
 
     # Add subplot labels
     subplotLabel(ax)
-   
-    data = import_scib_data(dataname="Stimulation1")
+    
     data = import_scib_data(dataname="ImmuneHuman")
-    data = import_scib_data(dataname="Stimulation2")
     data = import_scib_data(dataname="ImmuneHumanMouse")
+    data = import_scib_data(dataname="Stimulation1")
+    data = import_scib_data(dataname="Stimulation2")
     data = import_scib_data(dataname="Pancreas")
 
     rank = 2
@@ -27,6 +27,6 @@ def makeFigure():
         random_state=1,
     )
 
-    plotFactors(factors, data, ax[0:3], reorder=(0, 2), trim=(2,))
+    plotFactors(factors, data, ax[0:3])
     
     return f
--- sccp/imports/scib.py
+++ sccp/imports/scib.py
@@ -5,13 +5,16 @@ def import_scib_data(dataname):
     """Immune cell import from Theis lab: scib pipeline"""
     if dataname == "ImmuneHuman":
         annData = anndata.read_h5ad("/opt/andrew/scib/Immune_ALL_human.h5ad")
+        return tensorFy(annData, "batch")
     elif dataname == "ImmuneHumanMouse":
         annData = anndata.read_h5ad("/opt/andrew/scib/Immune_ALL_hum_mou.h5ad")
+        return tensorFy(annData, "batch")
     elif dataname == "Stimulation1":
         annData = anndata.read_h5ad("/opt/andrew/scib/sim1_1_norm.h5ad")
+        return tensorFy(annData, "Batch")
     elif dataname == "Stimulation2":
         annData = anndata.read_h5ad("/opt/andrew/scib/sim2_norm.h5ad")
+        return tensorFy(annData, "Batch")
     elif dataname == "Pancreas":
         annData = anndata.read_h5ad("/opt/andrew/scib/human_pancreas_norm_complexBatch.h5ad")
-    
-    return tensorFy(annData, "batch")
+        return tensorFy(annData, "tech")
