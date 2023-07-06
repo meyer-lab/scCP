4107380173170b363139471ad6bb6663bdfccb56
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-07-06 16:00:36 -0700

    Plotting all metrics for Pf2 and SCIB

---
+++ sccp/figures/figure10.py
@@ -0,0 +1,19 @@
+"""Investigating metrics for datasets"""
+from .common import (subplotLabel, getSetup, 
+   plotMetricSCIB)
+from ..imports.scib import  import_scib_metrics
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((25, 25), (4, 2))
+
+    # Add subplot labels
+    subplotLabel(ax)
+    
+    metricsDF, sheetName = import_scib_metrics()
+    
+    plotMetricSCIB(metricsDF, sheetName, ax[0:7])
+
+
+    return f
