1c882ecc90bdb0256ccac94e390c67228dd1088e
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-07-11 21:47:44 -0700

    Investigating cells for each cell state

---
+++ sccp/figures/figure4a.py
@@ -0,0 +1,41 @@
+"""
+Parafac2 implementation on PBMCs treated wtih PopAlign/Thompson drugs
+"""
+from .common import subplotLabel, getSetup, plotCmpUMAP
+from ..imports.scRNA import ThompsonXA_SCGenes
+from ..parafac2 import parafac2_nd
+import umap 
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((15, 8), (2, 2))
+
+    # Add subplot labels
+    subplotLabel(ax)
+
+    # Import of single cells: [Drug, Cell, Gene]
+    data = ThompsonXA_SCGenes()
+    rank = 30
+
+    weight, factors, projs, _ = parafac2_nd(
+        data,
+        rank=rank,
+        random_state=1,
+    )
+    
+    # UMAP dimension reduction
+    pf2Points = umap.UMAP(random_state=1).fit(np.concatenate(projs, axis=0))
+
+    cellState = 30; cmp = 30
+    plotCmpUMAP(cellState, cmp, factors, pf2Points, projs, ax[0])
+    
+    cellState = 30; cmp = 16
+    plotCmpUMAP(cellState, cmp, factors, pf2Points, projs, ax[1])
+    
+    cellState = 30; cmp = 17
+    plotCmpUMAP(cellState, cmp, factors, pf2Points, projs, ax[2])
+    
+
+    return f
