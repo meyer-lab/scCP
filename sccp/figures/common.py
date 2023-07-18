Fix normalization (#155)
* Fix normalization

* Cleanup

* remove extra terms

---------

Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

sccp/figures/common.py
@@ -286,7 +286,7 @@ def plotCmpUMAP(cellState, cmp, factors, pf2Points, projs, ax):
         xlabel="UMAP1",
         title="Cell State:" + str(cellState)+"- Component:" + str(cmp))
     
-def plotCmpUMAPDiv(cellState, cmp, factors, pf2Points, projs, f, ax):
+def plotCmpUMAPDiv(cellState, cmp, factors, pf2Points, projs, ax):
     """Scatterplot of UMAP visualization weighted by
     projections for a component and cell state"""
     cellSkip = 10 
@@ -294,17 +294,19 @@ def plotCmpUMAPDiv(cellState, cmp, factors, pf2Points, projs, f, ax):
     umap2 = pf2Points[::cellSkip, 1]
     allP = np.concatenate(projs, axis=0)
     weightedProjs = allP[:, cellState-1] * factors[1][cellState-1, cmp-1]
+    weightedProjs = weightedProjs[::cellSkip]
     weightedProjs = weightedProjs / np.max(np.abs(weightedProjs))
     cmap = sns.diverging_palette(240, 10, as_cmap=True)
-  
-    tl = ax.scatter(
+    psm = plt.pcolormesh([[-1, 1],[-1, 1]], cmap=cmap)
+    
+    ax.scatter(
             umap1,
             umap2,
-            c=weightedProjs[::cellSkip],
+            c=weightedProjs,
             cmap=cmap,
             s=0.2,
         )
-    f.colorbar(tl, ax=ax)
+    plt.colorbar(psm, ax=ax)
     
     ax.set(
         ylabel="UMAP2",

sccp/figures/figure4a.py
@@ -19,7 +19,7 @@ def makeFigure():
     # Import of single cells: [Drug, Cell, Gene]
     data = ThompsonXA_SCGenes()
     rank = 30
-
+    
     weight, factors, projs, _ = parafac2_nd(
         data,
         rank=rank,
@@ -29,10 +29,10 @@ def makeFigure():
     # UMAP dimension reduction
     pf2Points = umap.UMAP(random_state=1).fit_transform(np.concatenate(projs, axis=0))
     
-    cellState = np.arange(23, 31, 1) 
-    component = np.arange(23, 31, 1) 
+    cellState = np.arange(17, 25, 1) 
+    component = np.arange(17, 25, 1) 
     
     for i in range(len(cellState)):
-        plotCmpUMAPDiv(cellState[i], component[i], factors, pf2Points, projs, f, ax[i])
+        plotCmpUMAPDiv(cellState[i], component[i], factors, pf2Points, projs, ax[i])
 
     return f