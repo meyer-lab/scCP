85eb0f5e70ac4e654f5782f2a3e4e0a50414e2df
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-07-24 14:33:37 -0700

    Normalizing metrics from 0 to 1 and creating overall scores similar to SCIB

--- sccp/figures/common.py
+++ sccp/figures/common.py
@@ -426,6 +426,16 @@ def plotMetricSCIB(metricsDF, sheetName, axs):
         pc(datasetDF, "Metric", colormap=plt.get_cmap("Set1"), ax=axs[i])
         axs[i].tick_params(axis="x", rotation=45)
         axs[i].set(title=sheets)
+        
+def plotMetricNormSCIB(metricsDF, sheetName, axs):
+    """Plots overall metric values across SCIB and Pf2 for one dataset"""
+    for i, sheets in enumerate(sheetName):
+        datasetDF = metricsDF.loc[metricsDF["Dataset"] == sheets]
+        datasetDF = datasetDF.drop(columns="Dataset").reset_index(drop=True)
+        datasetDF = datasetDF.pivot_table(index="Metric", columns="Method", values="Value").reset_index()
+        pc(datasetDF, "Metric", colormap=plt.get_cmap("Set1"), ax=axs[i])
+        axs[i].tick_params(axis="x", rotation=45)
+        axs[i].set(title=sheets)
     
 def plotCellCount(dataDF, ax):
     """Plot number of cells per experiment for a dataframe"""
