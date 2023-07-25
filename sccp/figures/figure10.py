777849fa239161a08a6b23e47e85050f5965857d
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2023-07-24 17:15:10 -0700

    Normalizing metrics from 0 to 1 and creating overall scores similar to SCIB (#166)
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figure10.py
+++ sccp/figures/figure10.py
@@ -1,19 +1,21 @@
 """Investigating metrics for datasets"""
 from .common import (subplotLabel, getSetup, 
-   plotMetricSCIB)
-from ..imports.scib import  import_scib_metrics
+   plotMetricSCIB, plotMetricNormSCIB)
+from ..imports.scib import  import_scib_metrics, normalize_scib
 
 
 def makeFigure():
    """Get a list of the axis objects and create a figure."""
    # Get list of axis objects
-   ax, f = getSetup((18, 18), (4, 2))
+   ax, f = getSetup((30, 18), (4, 4))
 
    # Add subplot labels
    subplotLabel(ax)
    
-   metricsDF, sheetName = import_scib_metrics()
-
+   metricsDF, metricsName, sheetName = import_scib_metrics()
    plotMetricSCIB(metricsDF, sheetName, ax[0:7])
+   
+   metricsNormDF, metricsName, sheetName  = normalize_scib(metricsDF, metricsName, sheetName)
+   plotMetricNormSCIB(metricsNormDF, sheetName, ax[7:14])
 
    return f
--- sccp/imports/scib.py
+++ sccp/imports/scib.py
@@ -32,13 +32,10 @@ def import_scib_metrics():
     """Imports all metric outputs from SCIB including Pf2 for all datasets"""
     xls = pd.ExcelFile("sccp/data/ScibMetrics.xlsx")
     
-    # metrics = ["PCR batch", "Batch ASW", "graph iLISI", "graph connectivity", 
-    #            "kBET", "NMI cluster/label", "ARI cluster/label", 
-    #            "Cell type ASW", "isolated label F1", 
-    #            "isolated label silhouette", "graph cLISI"]
-
-    metrics = ["PCR batch", "Batch ASW", "graph iLISI",	
-               "graph connectivity", "kBET"]
+    metrics = ["PCR batch", "Batch ASW", "graph iLISI", "graph connectivity", 
+               "kBET", "NMI cluster/label", "ARI cluster/label", 
+               "Cell type ASW", "isolated label F1", 
+               "isolated label silhouette", "graph cLISI"]
     
     sheet_name=["immune_cell_hum","immune_cell_hum_mou", "simulations_1_1", 
                 "simulations_2", "pancreas", "mouse_brain_atac_genes_small", 
@@ -56,5 +53,40 @@ def import_scib_metrics():
 
     df = pd.melt(df, id_vars=["Dataset", "Method"], value_vars=metrics).rename(
             columns={"variable": "Metric", "value": "Value"})
-        
-    return df.dropna(), sheet_name
+    
+    df = df.dropna()
+    
+    return df, metrics, sheet_name
+    
+    
+def normalize_scib(df, metrics, sheet_name):
+    "Normalizes scores to overall scores for bioconservation and batch correction. References Theis Lab "
+    batchMetrics = ["PCR batch", "Batch ASW", "graph iLISI",	
+                "graph connectivity", "kBET"]
+
+    bioconsMetrics = ["NMI cluster/label", "ARI cluster/label", 
+                "Cell type ASW", "isolated label F1", 
+                "isolated label silhouette", "graph cLISI"]
+    
+    for i, sheet in enumerate(sheet_name): 
+        for j, metric in enumerate(metrics): 
+            allMethodsDF = df.loc[(df["Metric"] == metric) & (df["Dataset"] == sheet)]
+            normValues = (allMethodsDF["Value"] - np.min(allMethodsDF["Value"])) / (np.max(allMethodsDF["Value"]) - np.min(allMethodsDF["Value"]))
+            df.loc[(df["Metric"] == metric) & (df["Dataset"] == sheet)]["Value"] == normValues
+            
+    totalDF = pd.DataFrame([])
+    for i, sheet in enumerate(sheet_name): 
+        for j, method in enumerate(np.unique(df["Method"])): 
+            allMetricsDF = df.loc[(df["Method"] == method) & (df["Dataset"] == sheet)]
+            batchValue =  np.mean(allMetricsDF.loc[allMetricsDF["Metric"].isin(batchMetrics)]["Value"])
+            bioconsValue = np.mean(allMetricsDF.loc[allMetricsDF["Metric"].isin(bioconsMetrics)]["Value"])
+            overallValue = (0.4*batchValue) + (0.6*bioconsValue)
+
+            totalDF = pd.concat([totalDF, pd.DataFrame({"Dataset": sheet, "Method": method, "Overall": [overallValue], "BioConservation": [bioconsValue], "Batch": [batchValue]})])
+            
+            
+    totalDF = pd.melt(totalDF, id_vars=["Dataset", "Method"], value_vars=["Overall", "BioConservation", "Batch"]).rename(
+            columns={"variable": "Metric", "value": "Value"})
+    
+
+    return totalDF, metrics, sheet_name
