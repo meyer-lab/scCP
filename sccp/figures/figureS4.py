45ef2edfe846b6b7784a1a52d73658b24c58bf9c
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-08-07 11:46:18 -0700

    set up S4 for new comp set

--- sccp/figures/figureS4.py
+++ sccp/figures/figureS4.py
@@ -1,10 +1,10 @@
 """
-S16: what's going on in component 16
+S4: what's going on in component 11
 article: https://www.science.org/doi/10.1126/science.abf1970
 data: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE174188
 """
 
-# GOAL: figure out whats going on in component 16
+# GOAL: figure out whats going on in component 11
 
 # load functions/modules ----
 from .common import (
@@ -27,7 +27,7 @@ def makeFigure():
     # Add subplot labels
     subplotLabel(ax)
 
-    rank = 39
+    rank = 40
 
     lupus_tensor, _, _ = load_lupus_data() 
 
@@ -38,15 +38,15 @@ def makeFigure():
     C_matrix = pd.DataFrame(factors[2], columns = [f"comp_{i}" for i in np.arange(1, rank + 1)], index = genes)
 
     # threshold of 0.025 decided by looking at threshold of 0.01; then chose elbow
-    high_expr16 = C_matrix[C_matrix['comp_16'] > 0.025]['comp_16'].sort_values(ascending=False)
+    high_expr11 = C_matrix[C_matrix['comp_11'] > 0.025]['comp_11'].sort_values(ascending=False)
 
-    #high_expr16 = expr16[expr16['comp_16'] > 0.01]
-    print(high_expr16)
-    print(high_expr16.describe())
+    #high_expr16 = expr11[expr11['comp_11'] > 0.01]
+    print(high_expr11)
+    print(high_expr11.describe())
 
-    bar_data = pd.DataFrame(high_expr16).reset_index()
+    bar_data = pd.DataFrame(high_expr11).reset_index()
     print(bar_data)
-    sns.barplot(data = bar_data, x = 'index', y = 'comp_16', ax = ax[0])
+    sns.barplot(data = bar_data, x = 'index', y = 'comp_11', ax = ax[0])
     
     ax[0].tick_params(axis="x", rotation=90)
 
