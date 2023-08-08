ce1636ad7cdb7b67b1478e7af2f81aae0965147e
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-08-08 16:48:37 -0700

    update plotFactors + add data

--- sccp/figures/figureS1.py
+++ sccp/figures/figureS1.py
@@ -7,31 +7,27 @@ data: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE174188
 # GOAL: test Pf2 on lupus data, get visualizations for factor matrices
 
 # load functions/modules ----
-from .common import subplotLabel, getSetup, plotFactors, plotWeight
+from .common import subplotLabel, getSetup, plotFactors, plotWeight, openPf2
 from parafac2 import parafac2_nd
 from ..imports.scRNA import load_lupus_data
-from .common import subplotLabel, getSetup
+from .common import subplotLabel, getSetup, savePf2
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((12, 12), (2, 2))  # fig size  # grid size
+    ax, f = getSetup((12, 16), (2, 2))  # fig size  # grid size
 
     # Add subplot labels
     subplotLabel(ax)
 
-    rank = 5
+    rank = 40
 
     (
         lupus_tensor,
         obs,
-    ) = load_lupus_data(every_n = 10)  # so that it builds quickly
+    ) = load_lupus_data()  
 
-
-    weights, factors, _, _ = parafac2_nd(
-        lupus_tensor, rank=rank, n_iter_max=20, random_state=1
-    )
 
     status = (
         obs[["sample_ID", "SLE_status"]]
@@ -42,15 +38,20 @@ def makeFigure():
     bool = status['sample_ID'] == lupus_tensor.condition_labels
     assert bool.mean() == 1.0
 
+    group_labs = status.set_index("sample_ID")["SLE_status"]
 
-    lut = {"SLE": "c", "Healthy": "m"}
-    row_colors = status["SLE_status"].map(lut)
+    #weights, factors, projs, _ = parafac2_nd(
+    #    lupus_tensor, rank=rank, random_state=1, verbose = True
+    #)
+
+    #savePf2(weights, factors, projs, dataName = "lupus")
+    weights, factors, _ = openPf2(rank = rank, dataName = 'lupus', optProjs=True)
 
     plotFactors(
-        factors, lupus_tensor, ax[0:3], reorder=(0, 2), trim=(2,), row_colors=row_colors
+        factors, lupus_tensor, ax[0:3], reorder=(0, 2), trim=(2,), cond_group_labels = group_labs, saveGenes=True
     )
 
     plotWeight(weights, ax[3])
-    ax[3].set_title("Weight of Each Componenet")
+    ax[3].set_title("Weight of Each Component")
 
     return f
