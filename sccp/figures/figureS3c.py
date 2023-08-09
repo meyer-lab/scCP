bf8e7c0c3dc09f8eb84d3ed660c5430c0685071d
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-08-08 23:43:27 -0700

    plot ROC AUC of logistic model

---
+++ sccp/figures/figureS3c.py
@@ -0,0 +1,60 @@
+"""
+S3: Logisitic Regression (and maybe SVM) on Pf2 Factor matrix A output
+article: https://www.science.org/doi/10.1126/science.abf1970
+data: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE174188
+"""
+
+# GOAL: run logisitc regression and assess predictive ability of 39 comp Pf2 using ROC AUC
+# comparison to Perez et al (linked above) fig 4C
+
+# load functions/modules ----
+from .common import (
+    subplotLabel,
+    getSetup,
+    openPf2
+)
+from ..imports.scRNA import load_lupus_data
+from ..logisticReg import getPf2ROC
+from sklearn.metrics import RocCurveDisplay
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((12, 6), # fig size
+                     (1, 1) # grid size
+                     )
+
+    # Add subplot labels
+    subplotLabel(ax)
+
+    rank = 40
+
+    lupus_tensor, obs = load_lupus_data() 
+    
+    patients = lupus_tensor.condition_labels
+
+    status = (
+            obs[['sample_ID', 'SLE_status', 'Processing_Cohort']]
+            .drop_duplicates()
+    )
+
+    group_labs = status.set_index('sample_ID')[['SLE_status', 'Processing_Cohort']]
+    
+    _, factors, _, = openPf2(rank = rank, dataName = 'lupus', optProjs=True)
+
+    A_matrix = factors[0]
+
+    penalties_to_test = [10, 20, 30, 50, 100, 150, 200, 1000]
+
+    # get test data, and decisions made by the trained model corresponding to those test data
+    y_test, sle_decisions = getPf2ROC(A_matrix, patients, group_labs, rank, penalties_to_test=penalties_to_test)
+    
+    # make plot of ROC AUC
+    RocCurveDisplay.from_predictions(y_test, sle_decisions, 
+                                     pos_label = "SLE",
+                                     plot_chance_level = True,
+                                     ax = ax[0])
+    
+    ax[0].set_title('OOS ROC for Cases/Controls: ' + str(rank) + ' Component LASSO')
+
+    return f
--- sccp/logisticReg.py
+++ sccp/logisticReg.py
@@ -5,3 +5,38 @@ from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.model_selection import StratifiedGroupKFold
 from sklearn.metrics import roc_auc_score
 
+def getPf2ROC(A_matrix, conditions, condition_batch_labels, rank, penalties_to_test = 10):
+    """Train a logistic regression model using CV on some cohorts, test on another"""
+    A_matrix = pd.DataFrame(A_matrix, 
+                            index = conditions,
+                            columns = [f"comp_{i}" for i in np.arange(1, rank + 1)])
+    cond_labels = condition_batch_labels.reset_index()[['SLE_status', 'sample_ID']].drop_duplicates().set_index('sample_ID')
+    comps_w_sle_status = A_matrix.merge(cond_labels, left_index=True, right_index=True, how = 'left')
+    patients_in_batch_4 = condition_batch_labels[condition_batch_labels['Processing_Cohort'] == str(4.0)].index.tolist()
+    other_patients = []
+    for pat in conditions:
+        if pat not in patients_in_batch_4:
+            other_patients.append(pat)
+
+    assert len(conditions) == (len(patients_in_batch_4) + len(other_patients))
+
+    cohort_4 = comps_w_sle_status[comps_w_sle_status.index.isin(patients_in_batch_4)]
+    cohorts_123 = comps_w_sle_status[comps_w_sle_status.index.isin(other_patients)]
+    last_comp = "comp_" + str(rank)
+    cmp_train = cohort_4.loc[:, "comp_1":last_comp].to_numpy()
+    y_train = cohort_4.loc[:, "SLE_status"].to_numpy()
+    cmp_test = cohorts_123.loc[:, "comp_1":last_comp].to_numpy()
+    y_test = cohorts_123.loc[:, "SLE_status"].to_numpy()
+    # train a logisitic regression model using cross validation
+    log_reg = LogisticRegressionCV(random_state=0, max_iter = 10000, penalty = 'l1', solver = 'saga',
+                                   scoring = "roc_auc",
+                                    Cs = penalties_to_test)
+    log_fit = log_reg.fit(cmp_train, y_train)
+
+    # get decision function for ROC AUC
+    sle_decisions = log_fit.decision_function(cmp_test)
+    # validate the ROC AUC of the model
+    roc_auc = roc_auc_score(y_test, sle_decisions)
+    print("The best ROC AUC is: ", roc_auc)
+
+    return y_test, sle_decisions
