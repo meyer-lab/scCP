9db9b45bf826cc074833dfc6ef65e09cd6766ed2
Sean :) <126696611+pascoesean@users.noreply.github.com>
2023-08-10 13:51:18 -0700

    plot ROC AUC of logistic model (#186)
    * plot ROC AUC of logistic model
    
    * update train/test data (no leakage)
    
    * Pf2 names (#187)
    
    * Adding components by cell type and raw genes by cell type
    
    * Changing names of saved Pf2 results
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * Delete old Pf2 files (#188)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * update plotFactors + add lupus data (#181)
    
    * update plotFactors + add data
    
    * rename variable (easier to read)
    
    * Renaming saved Pf2 files
    
    * remove old/redundant code
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * make S2 umap; add other obs (#182)
    
    * make S2 umap; add other obs
    
    * change colors + titles
    
    * add open/save UMAP
    
    * remove extra imports
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    
    * Allow to use number for comps (#190)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * remove old files
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

---
+++ sccp/figures/figureS3c.py
@@ -0,0 +1,60 @@
+"""
+S3: Logisitic Regression (and maybe SVM) on Pf2 Factor matrix A output
+article: https://www.science.org/doi/10.1126/science.abf1970
+data: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE174188
+"""
+
+# GOAL: run logisitc regression and assess predictive ability of 40 comp Pf2 using ROC AUC
+# comparison to Perez et al (linked above) fig 4C
+
+# load functions/modules ----
+from .common import (
+    subplotLabel,
+    getSetup,
+    openPf2
+)
+from ..imports.scRNA import load_lupus_data
+from ..logisticReg import getPf2ROC
+from sklearn.metrics import RocCurveDisplay
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((12, 6), # fig size
+                     (1, 1) # grid size
+                     )
+
+    # Add subplot labels
+    subplotLabel(ax)
+
+    rank = 40
+
+    _, obs = load_lupus_data() 
+
+
+    status = (
+            obs[['sample_ID', 'SLE_status', 'Processing_Cohort', 'patient']]
+            .drop_duplicates()
+    )
+
+    group_labs = status.set_index('sample_ID')
+    
+    _, factors, _, = openPf2(rank = rank, dataName = 'lupus', optProjs=True)
+
+    A_matrix = factors[0]
+
+    # only doing 50 because that is the penalty we used when we chose 40 as an optimal component number
+    penalties_to_test = [50]
+
+    # get test data, and decisions made by the trained model corresponding to those test data
+    y_test, sle_decisions = getPf2ROC(A_matrix, group_labs, rank, penalties_to_test=penalties_to_test)
+    
+    # make plot of ROC AUC
+    RocCurveDisplay.from_predictions(y_test, sle_decisions, 
+                                     pos_label = "SLE",
+                                     plot_chance_level = True,
+                                     ax = ax[0])
+    
+    ax[0].set_title('OOS ROC for Cases/Controls: ' + str(rank) + ' Component LASSO')
+
+    return f
