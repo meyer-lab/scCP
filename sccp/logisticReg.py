0f39b80054ba4ccec0f0f5c3b3f23335f1b39ecc
Sean :) <126696611+pascoesean@users.noreply.github.com>
2023-08-10 13:51:18 -0700

    plot ROC AUC of logistic model (#186)
    * plot ROC AUC of logistic model
    
    * update train/test data (no leakage)
    
    * Pf2 names (#187)
    
    * Adding components by cell type and raw genes by cell type
    
    * Changing names of saved Pf2 results
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * Delete old Pf2 files (#188)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * update plotFactors + add lupus data (#181)
    
    * update plotFactors + add data
    
    * rename variable (easier to read)
    
    * Renaming saved Pf2 files
    
    * remove old/redundant code
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * make S2 umap; add other obs (#182)
    
    * make S2 umap; add other obs
    
    * change colors + titles
    
    * add open/save UMAP
    
    * remove extra imports
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    
    * Allow to use number for comps (#190)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * remove old files
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/logisticReg.py
+++ sccp/logisticReg.py
@@ -66,3 +66,53 @@ def testPf2Ranks(pfx2_data, condition_labels_all, ranks_to_test,
     # concatenate all the results into one frame for viewing:
 
     return pd.concat(results, ignore_index = True)
+def getPf2ROC(A_matrix, condition_batch_labels, rank, penalties_to_test = 10):
+    """Train a logistic regression model using CV on some cohorts, test on another"""
+    # get list of conditions, patients
+    conditions = condition_batch_labels.index
+    patients = condition_batch_labels['patient'].tolist()
+    # make a combined dataframe from which to draw samples after splitting into train/test data
+    A_matrix = pd.DataFrame(A_matrix, 
+                            index = conditions,
+                            columns = [f"comp_{i}" for i in np.arange(1, rank + 1)])
+    comps_w_sle_status = A_matrix.merge(condition_batch_labels, left_index=True, right_index=True)
+
+    # need two lists of patients: one that have 
+    patients_in_batch_4 = condition_batch_labels[condition_batch_labels['Processing_Cohort'] == str(4.0)]['patient'].tolist()
+    other_patients = []
+    for pat in set(patients):
+        if pat not in patients_in_batch_4:
+            other_patients.append(pat)
+
+    # using set to make sure we don't count duplicates in that list
+    assert len(set(patients)) == len(patients_in_batch_4) + len(other_patients)
+    # now we have two lists of patients, one which are patients with samples in cohort 4, one which is everyone else
+
+    group_4 = comps_w_sle_status[comps_w_sle_status['patient'].isin(patients_in_batch_4)]
+    group_4 = group_4[group_4['Processing_Cohort'] == str(4.0)]
+    assert group_4['Processing_Cohort'].nunique() == 1
+    group_123 = comps_w_sle_status[comps_w_sle_status['patient'].isin(other_patients)]
+    assert group_123['Processing_Cohort'].nunique() == 3
+    # NOW we have the two groups that we need-- one with only cohort 4 (n=96) and one with other samples from non-4 patients (n=228)
+    # need to separate these out into predictor sets (components) and target variable (SLE Status)
+    # also can rename the 4 group as training and the 123 group as testing
+
+    last_comp = "comp_" + str(rank)
+    cmp_train = group_4.loc[:, "comp_1":last_comp].to_numpy()
+    y_train = group_4.loc[:, "SLE_status"].to_numpy()
+    cmp_test = group_123.loc[:, "comp_1":last_comp].to_numpy()
+    y_test = group_123.loc[:, "SLE_status"].to_numpy()
+
+    # train + fit a logisitic regression model using cross validation ON ONLY THE TRAINING (GROUP 4) DATA
+    log_reg = LogisticRegressionCV(random_state=0, max_iter = 10000, penalty = 'l1', solver = 'saga',
+                                   scoring = "roc_auc",
+                                    Cs = penalties_to_test)
+    log_fit = log_reg.fit(cmp_train, y_train)
+
+    # get decision function for ROC AUC
+    sle_decisions = log_fit.decision_function(cmp_test)
+    # validate the ROC AUC of the model 
+    roc_auc = roc_auc_score(y_test, sle_decisions)
+    print("The best ROC AUC is: ", roc_auc)
+
+    return y_test, sle_decisions
