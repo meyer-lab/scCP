711d9e0986d36b257e6abe7267900b991fa32121
Sean :) <126696611+pascoesean@users.noreply.github.com>
2023-08-11 10:22:09 -0700

    test pf2 rank with logisitic reg (#184)
    * test pf2 rank with logreg
    
    * Pf2 names (#187)
    
    * Adding components by cell type and raw genes by cell type
    
    * Changing names of saved Pf2 results
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * Delete old Pf2 files (#188)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * update plotFactors + add lupus data (#181)
    
    * update plotFactors + add data
    
    * rename variable (easier to read)
    
    * Renaming saved Pf2 files
    
    * remove old/redundant code
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * make S2 umap; add other obs (#182)
    
    * make S2 umap; add other obs
    
    * change colors + titles
    
    * add open/save UMAP
    
    * remove extra imports
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    
    * Allow to use number for comps (#190)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * plot ROC AUC of logistic model (#186)
    
    * plot ROC AUC of logistic model
    
    * update train/test data (no leakage)
    
    * Pf2 names (#187)
    
    * Adding components by cell type and raw genes by cell type
    
    * Changing names of saved Pf2 results
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * Delete old Pf2 files (#188)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * update plotFactors + add lupus data (#181)
    
    * update plotFactors + add data
    
    * rename variable (easier to read)
    
    * Renaming saved Pf2 files
    
    * remove old/redundant code
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * make S2 umap; add other obs (#182)
    
    * make S2 umap; add other obs
    
    * change colors + titles
    
    * add open/save UMAP
    
    * remove extra imports
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    
    * Allow to use number for comps (#190)
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * remove old files
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
    
    * buff up function definitions
    
    * merge editor whoops
    
    ---------
    
    Co-authored-by: Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

---
+++ sccp/figures/figureS3.py
@@ -0,0 +1,48 @@
+"""
+S3: Logisitic Regression (and maybe SVM) on Pf2 Factor matrix A output
+article: https://www.science.org/doi/10.1126/science.abf1970
+data: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE174188
+"""
+
+# GOAL: Test various Pf2 ranks to see which best predicts disease status
+
+# load functions/modules ----
+from .common import (
+    subplotLabel,
+    getSetup,
+    plotPf2RankTest
+)
+from ..imports.scRNA import load_lupus_data
+from ..logisticReg import testPf2Ranks
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((8, 8), # fig size
+                     (1, 1) # grid size
+                     )
+
+    # Add subplot labels
+    subplotLabel(ax)
+
+    lupus_tensor, obs = load_lupus_data() # leaving in every_n for the github checks
+
+    status = (
+            obs[['sample_ID', 'SLE_status', 'Processing_Cohort']]
+            .drop_duplicates()
+        )
+    
+    group_labs = status.set_index('sample_ID')[['SLE_status', 'Processing_Cohort']]
+    
+
+    ranks_to_test = [2, 3] # set to 2,3 for github test
+    penalties_to_test = [10, 25, 50, 100, 200]
+
+    results = testPf2Ranks(lupus_tensor, group_labs, ranks_to_test, 
+                           penalties_to_test=penalties_to_test,
+                           cv_group='Processing_Cohort')
+    print(results)
+    plotPf2RankTest(results, ax[0])
+
+    return f
--- sccp/logisticReg.py
+++ sccp/logisticReg.py
@@ -1,3 +1,7 @@
+# LOGISTIC REGRESSION HELPER FUNCTIONS
+# for more information about possible inputs/specifications, see the sci-kit learn documentation:
+# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegressionCV.html
+
 import pandas as pd
 import numpy as np
 from parafac2 import parafac2_nd
@@ -5,6 +9,76 @@ from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.model_selection import StratifiedGroupKFold
 from sklearn.metrics import roc_auc_score
 
+def testPf2Ranks(pfx2_data, condition_labels_all, ranks_to_test,
+                 penalty_type = 'l1', solver = 'saga', error_metric = 'accuracy',
+                 penalties_to_test = 10, cv_group = None):
+    """Tests various numbers of components for Pf2 by optimizing some error metric in logisitic regression (predicting SLE status)
+    pfx2_data: data in Pf2X format
+    condition_labels_all: condition labels for both the thing you are predicting (like SLE Status) and your grouping variable (if applicable)
+    ranks_to_test: Pf2 ranks to try
+    penalty_type: type of logistic regression penalty (set to l1 [LASSO] as default, as this will scale unhelpful factors to 0)
+    solver: logistic regression solver to use. Saga default in case elastic net is needed
+    error_metric: error metric to pass to the logistic regression `scoring` parameter (https://scikit-learn.org/stable/modules/classes.html#module-sklearn.metrics)
+    penalties_to_test: list or int to pass to `Cs`: If int chooses int number of penalties to test
+    cv_group: (str) name of column in `condition_labels_all` that should be grouped by in cross validation
+    """
+    
+    results = []
+    for rank in ranks_to_test:
+
+        # perform pf2 on the given rank
+        print('########################################################################\n',
+              '########################################################################',
+              '\n\nPARAFAC2 FITTING: RANK ', str(rank))
+        _, factors, _, _ = parafac2_nd(pfx2_data, 
+                                rank = rank, 
+                                random_state = 1, 
+                                verbose=True)
+        
+        A_matrix = factors[0]
+
+        condition_labels = condition_labels_all['SLE_status']
+        
+        # train a logisitic regression model on that rank, using cross validation
+        # if we want cross validation groups made across a certain feature (like batch or patient); make them
+
+        if cv_group == None:
+            log_reg = LogisticRegressionCV(random_state=0, 
+                                       max_iter = 5000, 
+                                       penalty = penalty_type, 
+                                       solver = solver,
+                                       Cs = penalties_to_test,
+                                       scoring = error_metric)
+        else:
+            sgkf = StratifiedGroupKFold(n_splits=4)
+            # get labels for the group that you want to do cross validation by 
+            group_cond_labels = condition_labels_all[cv_group]
+
+            log_reg = LogisticRegressionCV(random_state=0, 
+                                        max_iter = 5000, 
+                                        cv= sgkf.split(A_matrix, condition_labels.to_numpy(), group_cond_labels.to_numpy()),
+                                        penalty = penalty_type, 
+                                        solver = solver,
+                                        Cs = penalties_to_test,
+                                        scoring = error_metric)
+        
+        log_fit = log_reg.fit(A_matrix, condition_labels.to_numpy())
+
+        acc_scores = pd.DataFrame(pd.DataFrame(log_fit.scores_.get('SLE')).mean()).rename(columns = {0: error_metric})
+        c_vals = pd.DataFrame(log_fit.Cs_).rename(columns = {0: "penalty"})
+
+        acc_w_c = acc_scores.merge(c_vals, left_index = True, right_index = True)
+
+        # grab fit results as a pandas dataframe, indicate which rank these are from
+        initial_results = pd.DataFrame(acc_w_c)
+        initial_results['rank'] = rank
+
+        # save best results into results list
+        results.append(initial_results)
+
+    # concatenate all the results into one frame for viewing:
+
+    return pd.concat(results, ignore_index = True)
 
 def getCompContribs(A_matrix, target, penalty_amt = 50):
     """Fit logistic regression model, return coefficients of that model"""
@@ -19,7 +93,12 @@ def getCompContribs(A_matrix, target, penalty_amt = 50):
     return coefs
 
 def getPf2ROC(A_matrix, condition_batch_labels, rank, penalties_to_test = 10):
-    """Train a logistic regression model using CV on some cohorts, test on another"""
+    """Train a logistic regression model using CV on some cohorts, test on another
+    A_matrix: first factor matrix (Pf2 output)
+    condition_batch_labels: unique list of observation categories, indexed by sample ID
+    rank: rank of Pf2 model being used
+    penalties_to_test: Penalties to be passed to `Cs` parameter of sklearn.linear_model.LogisticRegressionCV
+    """
     # get list of conditions, patients
     conditions = condition_batch_labels.index
     patients = condition_batch_labels['patient'].tolist()
@@ -29,7 +108,7 @@ def getPf2ROC(A_matrix, condition_batch_labels, rank, penalties_to_test = 10):
                             columns = [f"comp_{i}" for i in np.arange(1, rank + 1)])
     comps_w_sle_status = A_matrix.merge(condition_batch_labels, left_index=True, right_index=True)
 
-    # need two lists of patients: one that have 
+    # need two lists of patients: ones with batch 4 samples, and those that never had samples processed in batch 4
     patients_in_batch_4 = condition_batch_labels[condition_batch_labels['Processing_Cohort'] == str(4.0)]['patient'].tolist()
     other_patients = []
     for pat in set(patients):
