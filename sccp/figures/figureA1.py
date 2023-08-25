cbd229d4e199c6067b63d7bb8b60e02b299fec3d
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-08-25 12:31:50 -0700

    All code to import data in one anndata file

--- sccp/figures/figureA1.py
+++ sccp/figures/figureA1.py
@@ -17,6 +17,24 @@ def makeFigure():
     subplotLabel(ax)
     
     # barcodes = pd.read_csv("/opt/andrew/HamadCITEseq/control/barcodes.tsv.gz", sep="\t", header=None, names=("cell_barcode",))
+    # features = pd.read_csv("/opt/andrew/HamadCITEseq/control/features.tsv.gz", sep="\t", header=None)
+    # data = pd.DataFrame(scipy.io.mmread("/opt/andrew/HamadCITEseq/control/matrix.mtx.gz").todense())
+
+    # data["Expression Type"] = features.iloc[:, 2].values
+    # data["Expression Name"] = features.iloc[:, 1].values
+        
+    # geneDF = data.loc[data["Expression Type"] == "Gene Expression"].drop(columns="Expression Type").reset_index(drop=True)
+    # proteinDF = data.loc[data["Expression Type"] == "Antibody Capture"].drop(columns="Expression Type").reset_index(drop=True)
+    # geneNames = geneDF["Expression Name"].values
+    # print(len(geneNames))
+    
+    # genesAll = np.transpose(geneDF.drop(columns="Expression Name")).to_numpy()
+    # print(np.shape(genesAll))
+    # files = ["ic_pod1", "ic_pod7", "sc_pod1", "sc_pod1"]
+    # # files = ["ic_pod1"]
+    # numCells = [genesAll.shape[0]]
+    
+    # for i in range(len(files)):
     #     features = pd.read_csv("/opt/andrew/HamadCITEseq/"+files[i]+"/features.tsv.gz", sep="\t", header=None)
     #     data = pd.DataFrame(scipy.io.mmread("/opt/andrew/HamadCITEseq/"+files[i]+"/matrix.mtx.gz").todense())
     
@@ -24,33 +42,48 @@ def makeFigure():
     #     data["Expression Name"] = features.iloc[:, 1].values
         
     #     geneDF = data.loc[data["Expression Type"] == "Gene Expression"].drop(columns="Expression Type").reset_index(drop=True)
-    #     proteinDF = data.loc[data["Expression Type"] == "Antibody Capture"].drop(columns="Expression Type").reset_index(drop=True)
-    #     geneNames = geneDF["Expression Name"].values
-    
-    files = ["ic_pod1", "ic_pod1", "ic_pod1", "ic_pod1"]
-    
- 
- 
-    for i in range(len(files))
-    # barcodes = pd.read_csv("/opt/andrew/HamadCITEseq/control/barcodes.tsv.gz", sep="\t", header=None, names=("cell_barcode",))
-        features = pd.read_csv("/opt/andrew/HamadCITEseq/"+files[i]+"/features.tsv.gz", sep="\t", header=None)
-        data = pd.DataFrame(scipy.io.mmread("/opt/andrew/HamadCITEseq/"+files[i]+"/matrix.mtx.gz").todense())
-    
-        data["Expression Type"] = features.iloc[:, 2].values
-        data["Expression Name"] = features.iloc[:, 1].values
+    #     # proteinDF = data.loc[data["Expression Type"] == "Antibody Capture"].drop(columns="Expression Type").reset_index(drop=True)
         
-        geneDF = data.loc[data["Expression Type"] == "Gene Expression"].drop(columns="Expression Type").reset_index(drop=True)
-        proteinDF = data.loc[data["Expression Type"] == "Antibody Capture"].drop(columns="Expression Type").reset_index(drop=True)
-        geneNames = geneDF["Expression Name"].values
+    #     geneMatrix = np.transpose(geneDF.drop(columns="Expression Name")).to_numpy()
+        
+    #     genesAll = np.vstack((genesAll,geneMatrix))
+    #     numCells = np.append(numCells, geneMatrix.shape[0])
         
         
+    # files = ["control", "ic_pod1", "ic_pod7", "sc_pod1", "sc_pod7"]
     
     
-    geneDF = np.transpose(geneDF.drop(columns="Expression Name"))
-    condNames = []
-    np.repeat("Control", geneDF.shape[0])
-    print(geneDF.to_numpy())
-    # geneDF.columns = geneNames
+    # # files = ["control", "ic_pod1"]
+    
+    # print(numCells)
+    # print(np.shape(genesAll))
+    # print(np.shape(geneDF))
+    
+    # adata = anndata.AnnData(X = genesAll)
+    # adata.var_names = geneNames
+    # adata.obs["Condition"] = np.repeat(files, numCells)
+    
+
+    # adata.write_h5ad("file.h5ad")
+    data = anndata.read_h5ad("file.h5ad")
+    print(data)
+    
+    print(np.unique(data.obs["Condition"]))
+                     
+                    #   df.to_csv(join(path_here, "data/TopBotGenes_Cmp"+str(rank)+".csv"))
+    
+        
+        
+        
+        
+        
+    
+    
+    # geneDF = np.transpose(geneDF.drop(columns="Expression Name"))
+    # condNames = []
+    # np.repeat("Control", geneDF.shape[0])
+    # print(geneDF.to_numpy())
+    # # geneDF.columns = geneNames
     
     # print(geneDF)
     
@@ -68,7 +101,7 @@ def makeFigure():
     
  
     # geneNames = geneDF["Expression Name"].values
-    geneDF = np.transpose(geneDF.drop(columns="Expression Name"))
+    # geneDF = np.transpose(geneDF.drop(columns="Expression Name"))
     # geneDF.columns = geneNames
     
     
