777850eae14289f05a1b80a6fd65ef89b781dd61
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-08-25 11:38:23 -0700

    Saving work for importing data

--- sccp/figures/figureA1.py
+++ sccp/figures/figureA1.py
@@ -4,7 +4,8 @@ Parafac2 implementation on PBMCs treated wtih PopAlign/Thompson drugs
 from .common import (subplotLabel, getSetup)
 import numpy as np
 import pandas as pd
-import gzip
+import scipy.io
+import anndata
 
 
 def makeFigure():
@@ -15,17 +16,117 @@ def makeFigure():
     # Add subplot labels
     subplotLabel(ax)
     
+    # barcodes = pd.read_csv("/opt/andrew/HamadCITEseq/control/barcodes.tsv.gz", sep="\t", header=None, names=("cell_barcode",))
+    #     features = pd.read_csv("/opt/andrew/HamadCITEseq/"+files[i]+"/features.tsv.gz", sep="\t", header=None)
+    #     data = pd.DataFrame(scipy.io.mmread("/opt/andrew/HamadCITEseq/"+files[i]+"/matrix.mtx.gz").todense())
     
-    data1 = "/opt/andrew/HamadCITEseq/filtered_feature_bc_matrix_control/barcodes.tsv.gz"
-    df1 = pd.read_csv(data1, sep="\t", header=None)
-    print(df1)
-        # data, sep="\t", header=None, names=("cell_barcode",)
-    data2 = "/opt/andrew/HamadCITEseq/filtered_feature_bc_matrix_control/features.tsv.gz"
-    df2 = pd.read_csv(data2, sep="\t", header=None)
-    print(df2)
-    data3 = "/opt/andrew/HamadCITEseq/filtered_feature_bc_matrix_control/matrix.mtx.gz"
-    df3 = pd.read_csv(data3, sep="\t", header=None)
-    print(df3)
+    #     data["Expression Type"] = features.iloc[:, 2].values
+    #     data["Expression Name"] = features.iloc[:, 1].values
+        
+    #     geneDF = data.loc[data["Expression Type"] == "Gene Expression"].drop(columns="Expression Type").reset_index(drop=True)
+    #     proteinDF = data.loc[data["Expression Type"] == "Antibody Capture"].drop(columns="Expression Type").reset_index(drop=True)
+    #     geneNames = geneDF["Expression Name"].values
+    
+    files = ["ic_pod1", "ic_pod1", "ic_pod1", "ic_pod1"]
+    
+ 
+ 
+    for i in range(len(files))
+    # barcodes = pd.read_csv("/opt/andrew/HamadCITEseq/control/barcodes.tsv.gz", sep="\t", header=None, names=("cell_barcode",))
+        features = pd.read_csv("/opt/andrew/HamadCITEseq/"+files[i]+"/features.tsv.gz", sep="\t", header=None)
+        data = pd.DataFrame(scipy.io.mmread("/opt/andrew/HamadCITEseq/"+files[i]+"/matrix.mtx.gz").todense())
+    
+        data["Expression Type"] = features.iloc[:, 2].values
+        data["Expression Name"] = features.iloc[:, 1].values
+        
+        geneDF = data.loc[data["Expression Type"] == "Gene Expression"].drop(columns="Expression Type").reset_index(drop=True)
+        proteinDF = data.loc[data["Expression Type"] == "Antibody Capture"].drop(columns="Expression Type").reset_index(drop=True)
+        geneNames = geneDF["Expression Name"].values
+        
+        
+    
+    
+    geneDF = np.transpose(geneDF.drop(columns="Expression Name"))
+    condNames = []
+    np.repeat("Control", geneDF.shape[0])
+    print(geneDF.to_numpy())
+    # geneDF.columns = geneNames
+    
+    # print(geneDF)
+    
+    
+    # features = pd.read_csv("/opt/andrew/HamadCITEseq/ic_pod1/features.tsv.gz", sep="\t", header=None)
+    # data = pd.DataFrame(scipy.io.mmread("/opt/andrew/HamadCITEseq/ic_pod1/matrix.mtx.gz").todense())
+    
+    # data["Expression Type"] = features.iloc[:, 2].values
+    # data["Expression Name"] = features.iloc[:, 1].values
+    
+    # geneDF = data.loc[data["Expression Type"] == "Gene Expression"].drop(columns="Expression Type").reset_index(drop=True)
+    # proteinDF = data.loc[data["Expression Type"] == "Antibody Capture"].drop(columns="Expression Type").reset_index(drop=True)
+    
+    # print(geneDF)
+    
+ 
+    # geneNames = geneDF["Expression Name"].values
+    geneDF = np.transpose(geneDF.drop(columns="Expression Name"))
+    # geneDF.columns = geneNames
+    
+    
+    
+ 
+    
+    # np.vstack((a,b))
+
+
+    # assert gnees/proteins are the same for all experiments 
+
+    
+    
+    # adata = anndata.AnnData(X = geneDF.to_numpy())
+    # adata.var_names = geneDF.columns
+    
+    # adata
+    
+    # print(len(adata.var_names))
+    # adata.var = "Gene"
+  
+    
+    # data = np.transpose(data)
+    # data.columns = features.iloc[:, 1].values
+    
+    
+    # # data["Expression Type"] = features.iloc[:, 2].values
+    # # data["Expression Name"] = features.iloc[:, 1].values
+    # # data.reset_index().set_index("Expression Name", inplace=True)
+    # data["Condition"] = np.repeat("Control", len(barcodes.values))
+    # data
+    
+    # data = np.transpose(data)
+    
+    # print(data)
+    
+    # data.columns = barcodes.values
+    # data = 
+    # data.columns = features.iloc[:,1].values
+    # ad.AnnData(X=data.to_m())
+
+
+    # adata = anndata.AnnData(X = np.transpose(data).to_numpy(), var="Genes, var_names = 
+    # print(adata)
+    # print(adata.X)
+    # # var = features.iloc[:,1].values)
+    # adata.obs["Expression Type"] = features.iloc[:, 1].values
+    
+
+    
+    
+    # print(adata.X)
+    # data["Expression Type"] = features.iloc[:, 2].values
+    # data["Expression Name"] = features.iloc[:, 1].values
+    # data = np.transpose(data)
+    # print(data)
+
+
 
  
 
