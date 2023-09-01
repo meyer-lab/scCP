c70886209c52512ead7658e0dbddc22f997d0145
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-09-01 15:34:00 -0700

    Code for plots

--- sccp/figures/figure4c.py
+++ sccp/figures/figure4c.py
@@ -16,7 +16,7 @@ import pandas as pd
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((18, 18), (4, 4))
+    ax, f = getSetup((16, 16), (4, 4))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -24,6 +24,9 @@ def makeFigure():
     # Import of single cells: [Drug, Cell, Gene]
     data = ThompsonXA_SCGenes()
     dataDF = flattenData(data)
+    
+    
+    
     
     rank = 30
     dataDF["Cell Type"] = gateThomsonCells(rank=rank, saveCellTypes=False)
@@ -38,7 +41,7 @@ def makeFigure():
     weightedProjDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
     dataDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
     
-    comps = [5, 12, 20, 30]
+    comps = [12, 12, 12, 12, 12]
     for i, comp in enumerate(comps):
         plotCmpPerCellType(weightedProjDF, comps[i], ax[(2*i)+1], outliers=False)
         plotCmpUMAP(comps[i], factors, pf2Points, projs, ax[(2*i)+2])
@@ -49,14 +52,14 @@ def makeFigure():
     set3 = ["VPREB3", "CD79A", "FAM111B", "HOPX", "SLC30A3", "MS4A1"]
     set4 = ["CD163", "ADORA3", "MS4A6A", "RNASE1", "MTMR11"]
     
-    genes = [set1, set2, set3, set4]
-    for i in range(len(genes)):
-        data = pd.melt(dataDF, id_vars=["Condition", "Cell Type"], value_vars=genes[i]).rename(
-                columns={"variable": "Gene", "value": "Value"})
-        df = data.groupby(["Condition", "Cell Type", "Gene"]).mean()
+    # genes = [set1, set2, set3, set4]
+    # for i in range(len(genes)):
+    #     data = pd.melt(dataDF, id_vars=["Condition", "Cell Type"], value_vars=genes[i]).rename(
+    #             columns={"variable": "Gene", "value": "Value"})
+    #     df = data.groupby(["Condition", "Cell Type", "Gene"]).mean()
 
-        df = df.rename(columns={"Value": "Average Gene Expression For Drugs"})
-        sns.stripplot(data=df, x="Gene", y="Average Gene Expression For Drugs", hue="Cell Type", dodge=True, jitter=False, ax=ax[i+9])
+    #     df = df.rename(columns={"Value": "Average Gene Expression For Drugs"})
+    #     sns.stripplot(data=df, x="Gene", y="Average Gene Expression For Drugs", hue="Cell Type", dodge=True, jitter=False, ax=ax[i+9])
     
     
     
