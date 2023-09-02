f5c5bf1a4ca9e8c08668a3271e70f476c795281d
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-09-01 17:08:11 -0700

    Save changes to umap

--- sccp/figures/figure4c.py
+++ sccp/figures/figure4c.py
@@ -12,10 +12,12 @@ from ..imports.gating import gateThomsonCells
 import pandas as pd
 
 
+
+
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((16, 16), (4, 4))
+    ax, f = getSetup((40, 40), (5, 4))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -30,21 +32,26 @@ def makeFigure():
     _, factors, projs = openPf2(rank, "Thomson")
     pf2Points = openUMAP(rank, "Thomson", opt=False)
     
-    plotCellTypeUMAP(pf2Points, dataDF, ax[0])
+    # plotCellTypeUMAP(pf2Points, dataDF, ax[0])
         
     weightedProjDF = flattenWeightedProjs(data, factors, projs)
     weightedProjDF["Cell Type"] = dataDF["Cell Type"].values
     weightedProjDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
     dataDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
     
-    comps = [5, 12, 20, 30]
+    comps = [5, 5, 5, 5, 12, 12, 12, 12, 20, 20, 20, 20, 30, 30, 30, 30]
     for i, comp in enumerate(comps):
-        plotCmpPerCellType(weightedProjDF, comps[i], ax[i+1])
+        # plotCmpUMAP(weightedProjDF, comps[i], ax[i+1])
+        plotCmpUMAP(comp, factors, pf2Points, projs, ax[i])
+        
 
-    genes = ["NKG7", "GNLY", "MS4A1", "CD79A"]
-    for i, gene in enumerate(genes):
-        plotGenePerCellType(dataDF[(dataDF["Condition"].isin(["Budesonide", "CTRL3"]))], gene, ax[(2*i)+5])
-        plotGenePerCellType(dataDF[(dataDF["Condition"].isin(["Triamcinolone Acetonide", "CTRL1"]))], gene, ax[(2*i)+6])
+
+
+
+    # genes = ["NKG7", "GNLY", "MS4A1", "CD79A"]
+    # for i, gene in enumerate(genes):
+    #     plotGenePerCellType(dataDF[(dataDF["Condition"].isin(["Budesonide", "CTRL3"]))], gene, ax[(2*i)+5])
+    #     plotGenePerCellType(dataDF[(dataDF["Condition"].isin(["Triamcinolone Acetonide", "CTRL1"]))], gene, ax[(2*i)+6])
     
     
     return f
--- sccp/figures/figureS2.py
+++ sccp/figures/figureS2.py
@@ -20,7 +20,7 @@ from ..imports.scRNA import load_lupus_data
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((15, 13), (2, 2))
+    ax, f = getSetup((15, 13), (4, 4))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -31,11 +31,11 @@ def makeFigure():
     # Import of data
     _, obs= load_lupus_data()
     
-    broad_type = obs["cell_type_broad"].reset_index(drop=True)
-    lympho_type = obs["cell_type_lympho"].reset_index(drop=True)
+    # broad_type = obs["cell_type_broad"].reset_index(drop=True)
+    # lympho_type = obs["cell_type_lympho"].reset_index(drop=True)
 
     # replace NaN with string
-    lympho_type = lympho_type.cat.add_categories('other').fillna('other')
+    # lympho_type = lympho_type.cat.add_categories('other').fillna('other')
 
     _, factors, projs, = openPf2(rank = rank, dataName = 'lupus', optProjs=True)
 
@@ -44,8 +44,13 @@ def makeFigure():
     pf2Points = openUMAP(40, 'lupus', opt = True)
 
 
-    plotCmpUMAP(cmp, factors, pf2Points, projs, ax[0])
-    plotUMAP_obslabel(broad_type, pf2Points, ax[1])
-    plotUMAP_obslabel(lympho_type, pf2Points, ax[2])
+    comps = [5, 5, 5, 12, 12, 12, 20, 20, 20, 30, 30, 30]
+    for i, comp in enumerate(comps):
+        # plotCmpUMAP(weightedProjDF, comps[i], ax[i+1])
+        plotCmpUMAP(comp, factors, pf2Points, projs, ax[i])
+        
+    # plotCmpUMAP(cmp, factors, pf2Points, projs, ax[0])
+    # plotUMAP_obslabel(broad_type, pf2Points, ax[1])
+    # plotUMAP_obslabel(lympho_type, pf2Points, ax[2])
 
     return f
