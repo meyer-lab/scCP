61d7d70ff53b6384bc0bf2ef257a67afc4b7ff74
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2023-09-08 14:27:58 -0700

    Adding figures looking into weighted projections (#216)
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

---
+++ sccp/figures/figureCITEseq2.py
@@ -0,0 +1,40 @@
+"""
+Hamad CITEseq dataset
+"""
+from .common import (
+    subplotLabel,
+    getSetup,
+)
+import umap
+from ..imports.citeseq import import_citeseq, combine_all_citeseq
+from parafac2 import parafac2_nd
+import numpy as np
+from .commonFuncs.plotUMAP import (
+    plotCmpUMAP,
+)
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((50, 50), (5, 5))
+
+    # Add subplot labels
+    subplotLabel(ax)
+    data = import_citeseq()
+    rank = 40
+
+    _, factors, projs, _ = parafac2_nd(
+        data,
+        rank=rank,
+        random_state=1,
+    
+    )
+
+    pf2Points = umap.UMAP(random_state=1).fit(np.concatenate(projs, axis=0))
+
+    component = np.arange(1, 25, 1)
+
+    for i in range(len(component)):
+        plotCmpUMAP(component[i], factors, pf2Points, np.concatenate(projs, axis=0), ax[i])
+
+    return f
