5df56eedcf279ecd8460a2488a08c085714a6298
Nathaniel Thomas <catchnate@gmail.com>
2023-09-12 15:10:48 -0700

    Plot UMAP and save R2X while dropping cell type with certain conditions

--- sccp/figures/figure4.py
+++ sccp/figures/figure4.py
@@ -10,11 +10,12 @@ from ..imports.gating import gateThomsonCells
 from parafac2 import parafac2_nd
 import umap
 import numpy as np
+import json
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((18,16), (2, 3))
+    ax, f = getSetup((36,32), (7, 7))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -24,26 +25,31 @@ def makeFigure():
     rank = 30
     data.obs["cell_type"] = gateThomsonCells(rank)
     i = 0
+    r2x_values = {}
 
-    for type in data.obs.cell_type.unique()[1:]:
-        print(type)
-        print(len(data))
-        data_subset = data[data.obs.cell_type != type]
-        print(len(data_subset))
-        weight, factors, projs, _ = parafac2_nd(
-            tensorFy(data_subset, "Drugs"),
-            rank=rank,
-            random_state=1,
-            )
-        dataDF = flattenData(tensorFy(data_subset, "Drugs"))
-        dataDF['Cell Type'] = data_subset.obs['cell_type'].values
-        # plotFactorsRelaxed(factors, data_subset, ax[0:3], reorder=(0, 2), trim=(2,), saveGenes=True)
-        plotCellTypeUMAP(umap.UMAP(random_state=1).fit(np.concatenate(projs, axis=0)), dataDF, ax[i])
-        i += 1
-    
-
-    
-    return f
+    for cell_type in data.obs.cell_type.unique()[4:]:
+        for drug in data.obs.Drugs.unique():
+            print(cell_type)
+            print(len(data))
+            print(drug)
+            data_subset = data[(data.obs.cell_type != cell_type) | (data.obs.Drugs != drug)]
+            print(len(data_subset))
+            dataDF = tensorFy(data_subset, "Drugs")
+            weight, factors, projs, r2x = parafac2_nd(
+                dataDF,
+                rank=rank,
+                random_state=1,
+                )
+            dataDF = flattenData(dataDF)
+            dataDF['Cell Type'] = data_subset.obs['cell_type'].values
+            # plotFactorsRelaxed(factors, data_subset, ax[0:3], reorder=(0, 2), trim=(2,), saveGenes=True)
+            plotCellTypeUMAP(umap.UMAP(random_state=1).fit(np.concatenate(projs, axis=0)), dataDF, ax[i])
+            print(f'R2X while dropping {cell_type} across {drug} condition: {r2x}')
+            r2x_values[cell_type, drug] = r2x
+            i += 1
+        with open('./sccp/data/r2x_values.json', 'w+') as fp:
+            fp.write(str(r2x_values))
+        return f
 
     # weight, factors, projs, _ = parafac2_nd(
     #     data,
