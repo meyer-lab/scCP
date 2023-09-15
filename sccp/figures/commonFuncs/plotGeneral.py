3faa31cf4f26cc87df40f779115a4138f0a487b2
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-09-15 13:56:29 -0700

    Adding lupus genes from factors

--- sccp/figures/commonFuncs/plotGeneral.py
+++ sccp/figures/commonFuncs/plotGeneral.py
@@ -108,7 +108,7 @@ def plotGenePerCategCond(conds, categoryCond, genes, dataDF, axs):
         
 def plotGeneFactors(cmp, rank, dataName, axs, geneAmount=20):
     """Plotting weights for gene factors for both most negatively/positively weighted terms"""
-    df = pd.read_csv("sccp/data/"+dataName+"/TopBotGenes_Cmp"+str(rank)+".csv").rename(columns={"Unnamed: 0": "Gene"})
+    df = pd.read_csv("sccp/data/"+dataName+"/"+dataName+"TopBotGenes_Cmp"+str(rank)+".csv").rename(columns={"Unnamed: 0": "Gene"})
     cmpName = "Cmp. "+str(cmp)
     df = df[["Gene", cmpName]].sort_values(by=[cmpName])
     sns.barplot(data=df.iloc[:geneAmount,:], x="Gene", y=cmpName, color="k", ax=axs[0])
