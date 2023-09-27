df76f097914a70045b660e70fefffa9bdcdd6e38
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-09-27 10:41:01 -0700

    Adding GO for lupus data

---
+++ sccp/figures/figureLupus11.py
@@ -0,0 +1,26 @@
+"""Gene ontology for gene factors of Pf2"""
+from .common import (
+    subplotLabel,
+    getSetup,
+)
+from .commonFuncs.plotGeneOntology import plotCombGO, plotPvalGO
+from ..geneontology import geneOntology
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((20, 10), (2, 3))
+
+    # Add subplot labels
+    subplotLabel(ax)
+
+    # value = "Underexpressed"
+    value = "Overexpressed"
+
+    # combDF, pvalDF = geneOntology(cmpNumb=32, dataName="lupus", rank=40, geneAmount=20, goTerms=5, geneValue=value)
+
+    # plotCombGO(combDF, geneValue=value, axs=ax[0:3])
+    # plotPvalGO(pvalDF, geneValue=value, axs=ax[3:6])
+
+    return f
