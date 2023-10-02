de12fc1748023316b92890549e67fbd143fe7168
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-10-02 07:56:45 -0700

    Save changes

--- sccp/figures/figureThomson8.py
+++ sccp/figures/figureThomson8.py
@@ -40,14 +40,18 @@ def makeFigure():
     dataDF["Cell Type"] = gateThomsonCells(rank=rank, saveCellTypes=False)
 
     _, factors, projs = openPf2(rank, "Thomson")
-    pf2Points = openUMAP(rank, "Thomson", opt=False)
+    # pf2Points = openUMAP(rank, "Thomson", opt=False)
 
-    plotCellTypeUMAP(pf2Points, dataDF, ax[0])
+    # plotCellTypeUMAP(pf2Points, dataDF, ax[0])
 
     weightedProjDF = flattenWeightedProjs(data, factors, projs)
     weightedProjDF["Cell Type"] = dataDF["Cell Type"].values
     weightedProjDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
     dataDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
+    
+    threshold = -.3
+    cmp = 30
+    Wass_KL_Dist(threshold, cmp, data, dataDF, weightedProjDF)
 
     comps = [5, 12, 20, 30]
     
@@ -60,47 +64,82 @@ def makeFigure():
 
 
 
-def Wass_KL_Dist(ax, targCell, numFactors, RNA=False):
+def Wass_KL_Dist(threshold, cmp, data, dataDF, weightedProjDF):
     """Finds markers which have average greatest difference from other cells"""
-    for gene in dataDF.columns[:-2]:
-        choosecells(dataDF)
-        markAvg = np.mean(CITEseq[makrer])
+    for gene in data.variable_labels:
+        geneAvg = np.mean(dataDF[gene])   
+        cmpWeights = weightedProjDF[["Cell Type", "Cmp. "+str(cmp)]]
+        print(cmpWeights)
+        if threshold > 0: 
+            idxCells = np.argwhere(cmpWeights["Cmp. "+str(cmp)] > threshold)
+        else:
+            idxCells = np.argwhere(cmpWeights["Cmp. "+str(cmp)] < threshold)
         
-        specificcels = only choose cells whose weighted avergea is alge [gnees] /markAvg
-        off target = all other cells and divide by the markavg
-        if mean is greater than mean of off targe mean
+        geneDF = dataDF[["Cell Type", "Condition", gene]]
+        
+        
+        print(idxCells)
+        print(np.shape(idxCells))
+        
+        targCells = geneDF.iloc[idxCells.flatten(), :] 
+        targCells[gene] /= geneAvg
+        offCells = geneDFf[geneDF.index.difference(idxCells), ["Cell Type", "Condition", gene]] / geneAvg
+        
+        print(targCells)
+        print(offCells)
+        
+        # geneDF[(geneDF < weight).any(1)] / geneAvg
+        
+        # if np.mean(targCells) > np.mean(offCells):
+            
+        #         kdeTarg = KernelDensity(kernel='gaussian').fit(targCellMark.reshape(-1, 1))
+        #         kdeOffTarg = KernelDensity(kernel='gaussian').fit(offTargCellMark.reshape(-1, 1))
+        #         minVal = np.minimum(targCellMark.min(), offTargCellMark.min()) - 10
+        #         maxVal = np.maximum(targCellMark.max(), offTargCellMark.max()) + 10
+        #         outcomes = np.arange(minVal, maxVal + 1).reshape(-1, 1)
+        #         distTarg = np.exp(kdeTarg.score_samples(outcomes))
+        #         distOffTarg = np.exp(kdeOffTarg.score_samples(outcomes))
+        #         KL_div = stats.entropy(distOffTarg.flatten() + 1e-200, distTarg.flatten() + 1e-200, base=2)
+        #         markerDF = pd.concat([markerDF, pd.DataFrame({"Marker": [marker], "Wasserstein Distance": stats.wasserstein_distance(targCellMark, offTargCellMark), "KL Divergence": KL_div})])
+            
+        
+    #     cmpWeights 
+        
+    #     specificcels = only choose cells whose weighted avergea is alge [gnees] /markAvg
+    #     off target = all other cells and divide by the markavg
+    #     if mean is greater than mean of off targe mean
             
 
-    markerDF = pd.DataFrame(columns=["Marker", "Cell Type", "Amount"])
-    for marker in CITE_DF.loc[:, ((CITE_DF.columns != 'CellType1') & (CITE_DF.columns != 'CellType2') & (CITE_DF.columns != 'CellType3') & (CITE_DF.columns != 'Cell'))].columns:
-        markAvg = np.mean(CITE_DF[marker].values)
-        if markAvg > 0.0001:
-            targCellMark = CITE_DF.loc[CITE_DF["CellType2"] == targCell][marker].values / markAvg
-            offTargCellMark = CITE_DF.loc[CITE_DF["CellType2"] != targCell][marker].values / markAvg
-            if np.mean(targCellMark) > np.mean(offTargCellMark):
-                kdeTarg = KernelDensity(kernel='gaussian').fit(targCellMark.reshape(-1, 1))
-                kdeOffTarg = KernelDensity(kernel='gaussian').fit(offTargCellMark.reshape(-1, 1))
-                minVal = np.minimum(targCellMark.min(), offTargCellMark.min()) - 10
-                maxVal = np.maximum(targCellMark.max(), offTargCellMark.max()) + 10
-                outcomes = np.arange(minVal, maxVal + 1).reshape(-1, 1)
-                distTarg = np.exp(kdeTarg.score_samples(outcomes))
-                distOffTarg = np.exp(kdeOffTarg.score_samples(outcomes))
-                KL_div = stats.entropy(distOffTarg.flatten() + 1e-200, distTarg.flatten() + 1e-200, base=2)
-                markerDF = pd.concat([markerDF, pd.DataFrame({"Marker": [marker], "Wasserstein Distance": stats.wasserstein_distance(targCellMark, offTargCellMark), "KL Divergence": KL_div})])
+    # markerDF = pd.DataFrame(columns=["Marker", "Cell Type", "Amount"])
+    # for marker in CITE_DF.loc[:, ((CITE_DF.columns != 'CellType1') & (CITE_DF.columns != 'CellType2') & (CITE_DF.columns != 'CellType3') & (CITE_DF.columns != 'Cell'))].columns:
+    #     markAvg = np.mean(CITE_DF[marker].values)
+    #     if markAvg > 0.0001:
+    #         targCellMark = CITE_DF.loc[CITE_DF["CellType2"] == targCell][marker].values / markAvg
+    #         offTargCellMark = CITE_DF.loc[CITE_DF["CellType2"] != targCell][marker].values / markAvg
+    #         if np.mean(targCellMark) > np.mean(offTargCellMark):
+    #             kdeTarg = KernelDensity(kernel='gaussian').fit(targCellMark.reshape(-1, 1))
+    #             kdeOffTarg = KernelDensity(kernel='gaussian').fit(offTargCellMark.reshape(-1, 1))
+    #             minVal = np.minimum(targCellMark.min(), offTargCellMark.min()) - 10
+    #             maxVal = np.maximum(targCellMark.max(), offTargCellMark.max()) + 10
+    #             outcomes = np.arange(minVal, maxVal + 1).reshape(-1, 1)
+    #             distTarg = np.exp(kdeTarg.score_samples(outcomes))
+    #             distOffTarg = np.exp(kdeOffTarg.score_samples(outcomes))
+    #             KL_div = stats.entropy(distOffTarg.flatten() + 1e-200, distTarg.flatten() + 1e-200, base=2)
+    #             markerDF = pd.concat([markerDF, pd.DataFrame({"Marker": [marker], "Wasserstein Distance": stats.wasserstein_distance(targCellMark, offTargCellMark), "KL Divergence": KL_div})])
 
-    corrsDF = pd.DataFrame()
-    for i, distance in enumerate(["Wasserstein Distance", "KL Divergence"]):
-        ratioDF = markerDF.sort_values(by=distance)
-        posCorrs = ratioDF.tail(numFactors).Marker.values
-        corrsDF = pd.concat([corrsDF, pd.DataFrame({"Distance": distance, "Marker": posCorrs})])
-        markerDF = markerDF.loc[markerDF["Marker"].isin(posCorrs)]
-        sns.barplot(data=ratioDF.tail(numFactors), y="Marker", x=distance, ax=ax[i], color='k')
-        ax[i].set(xscale="log")
-        #ax[i].set_xticklabels(ax[i].get_xticklabels(), rotation=45)
-    if RNA:
-        ax[0].set(title="Wasserstein Distance - RNA")
-        ax[1].set(title="KL Divergence - RNA")
-    else:
-        ax[0].set(title="Wasserstein Distance - Surface Markers")
-        ax[1].set(title="KL Divergence - Surface Markers")
-    return corrsDF
+    # corrsDF = pd.DataFrame()
+    # for i, distance in enumerate(["Wasserstein Distance", "KL Divergence"]):
+    #     ratioDF = markerDF.sort_values(by=distance)
+    #     posCorrs = ratioDF.tail(numFactors).Marker.values
+    #     corrsDF = pd.concat([corrsDF, pd.DataFrame({"Distance": distance, "Marker": posCorrs})])
+    #     markerDF = markerDF.loc[markerDF["Marker"].isin(posCorrs)]
+    #     sns.barplot(data=ratioDF.tail(numFactors), y="Marker", x=distance, ax=ax[i], color='k')
+    #     ax[i].set(xscale="log")
+    #     #ax[i].set_xticklabels(ax[i].get_xticklabels(), rotation=45)
+    # if RNA:
+    #     ax[0].set(title="Wasserstein Distance - RNA")
+    #     ax[1].set(title="KL Divergence - RNA")
+    # else:
+    #     ax[0].set(title="Wasserstein Distance - Surface Markers")
+    #     ax[1].set(title="KL Divergence - Surface Markers")
+    # return corrsDF
