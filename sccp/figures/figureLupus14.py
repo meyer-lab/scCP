219df0373bde099b02563da1d018ac5be45c7e7d
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2023-10-05 16:02:04 -0700

    Creating plots for investigating raw data and weighted projections (#255)
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figureLupus14.py
+++ sccp/figures/figureLupus14.py
@@ -1,24 +1,30 @@
 """
 Plots all top and bottom genes for Thomson dataset
 """
-from .common import getSetup, openPf2, openUMAP
-from .commonFuncs.plotUMAP import plotCmpUMAP
+from .common import getSetup, openPf2, flattenWeightedProjs
+from .commonFuncs.plotUMAP import plotCmpPerCellType
 import numpy as np
+from ..imports.scRNA import load_lupus_data
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((28, 14), (5, 8))
+    ax, f = getSetup((14, 28), (8, 5))
 
     rank = 40
+    lupus_tensor, obs = load_lupus_data()
     _, factors, projs = openPf2(rank, "lupus", optProjs=True)
 
-    # UMAP dimension reduction
-    pf2Points = openUMAP(rank, "lupus", opt=True)
+     # get cell types
+    cell_types = obs[["cell_type_broad", "SLE_status"]].reset_index(drop=True)
+
+    weightedProjDF = flattenWeightedProjs(lupus_tensor, factors, projs)
+    weightedProjDF["Cell Type"] = cell_types["cell_type_broad"].values
+    weightedProjDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
 
     component = np.arange(1, rank + 1, 1)
-
-    for i in np.arange(0, rank):
-        plotCmpUMAP(component[i], factors[1], pf2Points, projs, ax[i])
+    for i, comp in enumerate(component):
+        plotCmpPerCellType(weightedProjDF, comp, ax[i], outliers=False)
+        
 
     return f
