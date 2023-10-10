85f2a30eaeae8e5a537087ec7b807bacd00962e1
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2023-10-10 11:53:56 -0700

    Fix tests for import func

--- sccp/tests/test_import.py
+++ sccp/tests/test_import.py
@@ -2,20 +2,20 @@
 Test the cross validation accuracy.
 """
 import numpy as np
-from ..imports.scRNA import ThompsonXA_SCGenes, load_lupus_data
+from ..imports.scRNA import import_thomson, import_lupus
 from ..imports.citeseq import import_citeseq
 
 
 def test_Thomson():
     """Test for correctness of cross validation."""
-    X = ThompsonXA_SCGenes()
+    X = import_thomson()
 
     assert np.all(np.isfinite(X.X))
 
 
 def test_Lupus():
     """Test for correctness of cross validation."""
-    X = load_lupus_data()
+    X = import_lupus()
 
     assert np.all(np.isfinite(X.X))
 
