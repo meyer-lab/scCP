431056a16ffb79d72d8f088674f01d52fd3b8f94
Aaron Meyer <ameyer@ucla.edu>
2023-10-19 07:11:36 -0700

    Fixed Lupus 12 and 14

--- sccp/figures/figureLupus14.py
+++ sccp/figures/figureLupus14.py
@@ -1,30 +1,18 @@
 """
 Plots all top and bottom genes for Thomson dataset
 """
-from .common import getSetup, openPf2, flattenWeightedProjs
+from .common import getSetup, openPf2
 from .commonFuncs.plotUMAP import plotCmpPerCellType
-import numpy as np
-from ..imports.scRNA import load_lupus_data
+
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
     ax, f = getSetup((14, 28), (8, 5))
 
-    rank = 40
-    lupus_tensor, obs = load_lupus_data()
-    _, factors, projs = openPf2(rank, "lupus", optProjs=True)
+    X = openPf2(rank=40, dataName="Lupus")
 
-     # get cell types
-    cell_types = obs[["cell_type_broad", "SLE_status"]].reset_index(drop=True)
-
-    weightedProjDF = flattenWeightedProjs(lupus_tensor, factors[1], projs)
-    weightedProjDF["Cell Type"] = cell_types["cell_type_broad"].values
-    weightedProjDF.sort_values(by=["Condition", "Cell Type"], inplace=True)
-
-    component = np.arange(1, rank + 1, 1)
-    for i, comp in enumerate(component):
-        plotCmpPerCellType(weightedProjDF, comp, ax[i], outliers=False)
-        
+    for i in range(40):
+        plotCmpPerCellType(X, i + 1, ax[i], outliers=False)
 
     return f
