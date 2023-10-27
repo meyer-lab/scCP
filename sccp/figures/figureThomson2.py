d25a3101fde48affe6040b43f6e84c8c074441ce
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2023-10-27 15:23:20 -0700

    Added Gene Signature Plots (#284)

--- sccp/figures/figureThomson2.py
+++ sccp/figures/figureThomson2.py
@@ -19,6 +19,9 @@ def makeFigure():
     subplotLabel(ax)
     rank = 30
     X = openPf2(rank, "Thomson")
+    print(X.uns["Pf2_A"].shape)
+    print(X.uns["Pf2_B"].shape)
+    print(X.varm["Pf2_C"].shape)
 
     gateThomsonCells(X)
     plotLabelsUMAP(X, "Cell Type", ax[0])
