0cb3ae07403c567b1e8c3a247a6d048a1ee4830d
Nathaniel Thomas <catchnate@gmail.com>
2023-10-31 21:39:55 -0700

    Update with new main branch updates

--- sccp/figures/figureThomsonDrop.py
+++ sccp/figures/figureThomsonDrop.py
@@ -1,33 +1,40 @@
 import numpy as np
 from .common import subplotLabel, getSetup, openPf2
-from ..imports.gating import gateThomsonCells
+from ..gating import gateThomsonCells
 from ..parafac2 import pf2
 from .commonFuncs.plotUMAP import (
     plotLabelsUMAP,
+    
+)
+from .commonFuncs.plotFactors import (
+    plotFactors,
 )
 
 
 def makeFigure():
     rank = 30
     data = openPf2(rank, "Thomson")
-    data.obs["cell_type"] = gateThomsonCells(data)
+    gateThomsonCells(data)
 
-    ax, f = getSetup((10,7), (1, 1))
+    ax, f = getSetup((16,12), (2, 2))
     subplotLabel(ax)
 
-    sampled_data = data.copy()
-    for drug in data.obs.Condition.unique():
-        for cell_type in data.obs.cell_type.unique():
-            filt = (sampled_data.obs.cell_type != cell_type) | (
-                    sampled_data.obs.Condition != drug
-                )
-            filt[
-                    np.random.choice(filt[~filt].index, int(len(filt[~filt].index) * 0.99), replace=False)
-                ] = True
-            sampled_data = sampled_data[filt]
+    sampled_data = data[(data.obs['Cell Type'] != 'Monocytes') | (data.obs['Condition'] != 'CTRL4')]
+
+    # sampled_data = data.to_memory(copy=True)
+    # for drug in data.obs.Condition.unique():
+    #     for cell_type in data.obs['Cell Type'].unique():
+    #         filt = (sampled_data.obs['Cell Type'] != cell_type) | (
+    #                 sampled_data.obs.Condition != drug
+    #             )
+    #         filt[
+    #                 np.random.choice(filt[~filt].index, int(len(filt[~filt].index) * 0.99), replace=False)
+    #             ] = True
+    #         sampled_data = sampled_data[filt]
 
     sampledX = pf2(sampled_data, "Condition", rank)
+    factors = [sampledX.uns["Pf2_A"], sampledX.uns["Pf2_B"], sampledX.varm["Pf2_C"]]
 
-    plotLabelsUMAP(sampledX, "cell_type", ax[0])
+    plotFactors(factors, sampledX, ax[0:3], reorder=(0, 2), trim=(2,))
 
     return f
