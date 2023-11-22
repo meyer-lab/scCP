fe470d5073c454af37b824f5cc9b91fc07a612d4
Aaron Meyer <ameyer@ucla.edu>
2023-11-22 13:24:31 -0800

    Works great

--- sccp/figures/figureThomsonDrop.py
+++ sccp/figures/figureThomsonDrop.py
@@ -1,40 +1,33 @@
-import numpy as np
-from .common import subplotLabel, getSetup, openPf2
+from .common import getSetup
 from ..gating import gateThomsonCells
-from ..parafac2 import pf2
-from .commonFuncs.plotGeneral import plotGeneFactors
-from .commonFuncs.plotUMAP import (
-    plotLabelsUMAP,
-)
+from ..factorization import pf2
 from .commonFuncs.plotFactors import (
     plotFactors,
 )
 from ..imports import import_thomson
-from ..imports import import_thomson
 
 
 def makeFigure():
-    rank = 30
+    rank = 20
     data = import_thomson()
     gateThomsonCells(data)
-
-    ax, f = getSetup((24, 24), (8, 8))
-
-    # data = data.to_memory(copy=True)
 
     sampled_data = data[
         (data.obs["Cell Type"] != "T Cells") | (data.obs["Condition"] != "CTRL4")
     ]
 
-    sampled_data = data
+    ax, f = getSetup((12, 12), (2, 3))
 
-    sampledX = pf2(sampled_data, rank)
+    origX = pf2(data, rank, doEmbedding=False)
+
+    factors = [origX.uns["Pf2_A"], origX.uns["Pf2_B"], origX.varm["Pf2_C"]]
+    plotFactors(factors, origX, ax[0:3], reorder=(0, 2), trim=(2,))
+
+    # sampled_data = data
+
+    sampledX = pf2(sampled_data, rank, doEmbedding=False)
     factors = [sampledX.uns["Pf2_A"], sampledX.uns["Pf2_B"], sampledX.varm["Pf2_C"]]
 
-    plotFactors(factors, sampledX, ax[0:3], reorder=(0, 2), trim=(2,))
-    plotLabelsUMAP(sampledX, "Cell Type", ax[3])
-
-    for i in np.arange(0, rank):
-        plotGeneFactors(i + 1, sampledX, ax[2 * i + 4 : 2 * i + 6], geneAmount=5)
+    plotFactors(factors, sampledX, ax[3:6], reorder=(0, 2), trim=(2,))
 
     return f
