fe49ef9d9134dbc47028ea57561af478020408ec
Aaron Meyer <ameyer@ucla.edu>
2023-11-25 07:12:20 -0800

    Simplify ROC code

--- sccp/logisticReg.py
+++ sccp/logisticReg.py
@@ -72,7 +72,7 @@ def testPf2Ranks(
     return pd.concat(results, ignore_index=True)
 
 
-def getCompContribs(A_matrix, target, penalty_amt: float = 50) -> np.ndarray:
+def getCompContribs(A_matrix, target, penalty_amt: float = 50) -> pd.DataFrame:
     """Fit logistic regression model, return coefficients of that model"""
     log_fit = LogisticRegression(
         random_state=0, max_iter=5000, penalty="l1", solver="saga", C=penalty_amt
@@ -86,53 +86,17 @@ def getCompContribs(A_matrix, target, penalty_amt: float = 50) -> np.ndarray:
     return coefs
 
 
-def getPf2ROC(A_matrix, condition_batch_labels):
+def getPf2ROC(
+    A_matrix: np.ndarray, condition_batch_labels: pd.DataFrame
+) -> tuple[np.ndarray, np.ndarray]:
     """Train a logistic regression model using CV on some cohorts, test on another
     A_matrix: first factor matrix (Pf2 output)
     condition_batch_labels: unique list of observation categories, indexed by sample ID
     """
-    # get list of conditions, patients
-    conditions = condition_batch_labels.index
-    patients = condition_batch_labels["patient"].tolist()
-    # make a combined dataframe from which to draw samples after splitting into train/test data
-    A_matrix = pd.DataFrame(
-        A_matrix,
-        index=conditions,
-        columns=[f"comp_{i}" for i in np.arange(1, A_matrix.shape[1] + 1)],
+    cohort_four = (condition_batch_labels["Processing_Cohort"] == "4.0").to_numpy(
+        dtype=bool
     )
-    comps_w_sle_status = A_matrix.merge(
-        condition_batch_labels, left_index=True, right_index=True
-    )
-
-    # need two lists of patients: ones with batch 4 samples, and those that never had samples processed in batch 4
-    patients_in_batch_4 = condition_batch_labels[
-        condition_batch_labels["Processing_Cohort"] == str(4.0)
-    ]["patient"].tolist()
-    other_patients = []
-    for pat in set(patients):
-        if pat not in patients_in_batch_4:
-            other_patients.append(pat)
-
-    # using set to make sure we don't count duplicates in that list
-    assert len(set(patients)) == len(patients_in_batch_4) + len(other_patients)
-    # now we have two lists of patients, one which are patients with samples in cohort 4, one which is everyone else
-
-    group_4 = comps_w_sle_status[
-        comps_w_sle_status["patient"].isin(patients_in_batch_4)
-    ]
-    group_4 = group_4[group_4["Processing_Cohort"] == str(4.0)]
-    assert group_4["Processing_Cohort"].nunique() == 1
-    group_123 = comps_w_sle_status[comps_w_sle_status["patient"].isin(other_patients)]
-    assert group_123["Processing_Cohort"].nunique() == 3
-    # NOW we have the two groups that we need-- one with only cohort 4 (n=96) and one with other samples from non-4 patients (n=228)
-    # need to separate these out into predictor sets (components) and target variable (SLE Status)
-    # also can rename the 4 group as training and the 123 group as testing
-
-    last_comp = "comp_" + str(A_matrix.shape[1])
-    cmp_train = group_4.loc[:, "comp_1":last_comp].to_numpy()
-    y_train = group_4.loc[:, "SLE_status"].to_numpy()
-    cmp_test = group_123.loc[:, "comp_1":last_comp].to_numpy()
-    y_test = group_123.loc[:, "SLE_status"].to_numpy()
+    y = (condition_batch_labels["SLE_status"] == "SLE").to_numpy(dtype=bool)
 
     # train + fit a logisitic regression model using cross validation ON ONLY THE TRAINING (GROUP 4) DATA
     log_reg = LogisticRegressionCV(
@@ -142,10 +106,12 @@ def getPf2ROC(A_matrix, condition_batch_labels):
         solver="saga",
         scoring="roc_auc",
     )
-    log_fit = log_reg.fit(cmp_train, y_train)
+    log_fit = log_reg.fit(A_matrix[cohort_four], y[cohort_four])
 
     # get decision function for ROC AUC
-    sle_decisions = log_fit.decision_function(cmp_test)
+    sle_decisions = log_fit.decision_function(A_matrix[~cohort_four])
+    y_test = y[~cohort_four]
+
     # validate the ROC AUC of the model
     roc_auc = roc_auc_score(y_test, sle_decisions)
     print("The best ROC AUC is: ", roc_auc)
