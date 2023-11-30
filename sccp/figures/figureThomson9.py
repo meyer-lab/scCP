9efc27545b901f25934cd1974a0f3f4f455ae008
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-11-29 17:54:01 -0800

    Small Changes2

--- sccp/figures/figureThomson9.py
+++ sccp/figures/figureThomson9.py
@@ -10,7 +10,7 @@ from ..gating import gateThomsonCells
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((14, 14), (5, 8))
+    ax, f = getSetup((14, 14), (5, 6))
 
     X = anndata.read_h5ad("factor_cache/Thomson.h5ad", backed="r")
 
