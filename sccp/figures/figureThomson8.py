0bb3baa62d75dd22a5a7957704f0fbaa9d888dcb
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-11-29 17:53:47 -0800

    Small Changes

--- sccp/figures/figureThomson8.py
+++ sccp/figures/figureThomson8.py
@@ -9,7 +9,7 @@ from .commonFuncs.plotGeneral import plotGeneFactors
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((20, 24), (10, 8))
+    ax, f = getSetup((20, 24), (10, 6))
 
     X = anndata.read_h5ad("factor_cache/Thomson.h5ad", backed="r")
 
