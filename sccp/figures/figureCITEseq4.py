d5933700de634ba631ef9bf26bd80ec12ab84995
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2024-02-05 12:48:45 -0800

    Create conditions ratio, include geens plots and insert important components (#370)
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figureCITEseq4.py
+++ sccp/figures/figureCITEseq4.py
@@ -1,32 +1,27 @@
 """
-CITEseq: Plotting weighted projections per component
+CITEseq: Plotting genes per component
 """
 from anndata import read_h5ad
 from .common import (
     subplotLabel,
     getSetup,
 )
-from .commonFuncs.plotUMAP import (
-    plotCmpUMAP, plotCmpPerCellType
-)
+from .commonFuncs.plotGeneral import plotGeneFactors
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((20, 20), (4, 4))
+    ax, f = getSetup((10, 10), (4, 4))
 
     # Add subplot labels
     subplotLabel(ax)
 
     X = read_h5ad("factor_cache/CITEseq.h5ad", backed="r")
-    XX = read_h5ad("/opt/pf2/CITE_Neighbors.h5ad", backed="r")
-    X.obs["leiden"] = XX.obs["leiden"] 
-
     comps = [22, 33, 47, 48, 23, 31, 43]
 
     for i, cmp in enumerate(comps):
-        plotCmpPerCellType(X, cmp, ax[2*i], cellType="leiden")
-        plotCmpUMAP(X, cmp, ax[2*i+1], cbarMax=.25)
+        plotGeneFactors(cmp, X, ax[2*i], geneAmount=10, top=True)
+        plotGeneFactors(cmp, X, ax[2*i+1], geneAmount=10, top=False)
 
     return f
