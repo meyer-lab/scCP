13c29d6629b1376f53bd417a550ddc8955d4475c
Nathaniel Thomas <catchnate@gmail.com>
2024-02-13 15:56:54 -0800

    Finished ThomsonDrop figure with all cell type subpopulations

--- sccp/figures/figureThomsonDrop.py
+++ sccp/figures/figureThomsonDrop.py
@@ -6,6 +6,7 @@ from .commonFuncs.plotFactors import reorder_table
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from ..gating import marker_genes_1, marker_genes_2
 
 cmap = sns.diverging_palette(240, 10, as_cmap=True)
 
@@ -14,11 +15,9 @@ def makeFigure():
     rank = 20
     data = import_thomson()
 
-    sampled_data = data[
-        (data.obs["Cell Type"] != "B Cells") | (data.obs["Condition"] != "CTRL4")
-    ]
+    print(data.obs["Cell Type2"].unique())
 
-    ax, f = getSetup((20, 10), (1, 2))
+    ax, f = getSetup((20, 10 * 9), (1 * 9, 4))
     bCellGeneSet = [
         "PXK",
         "MS4A1",
@@ -31,31 +30,139 @@ def makeFigure():
         "CR2",
         "VPREB3",
     ]
+    NaiveBCellGeneSet = marker_genes_2["B Naive"]
+    NKCellGeneSet = np.unique(marker_genes_1["Natural Killers"] + marker_genes_2["NK"])
+    DCCellGeneSet = np.unique(
+        marker_genes_1["Dendritic cells"] + marker_genes_2["cDCs"]
+    )
+    pDCGeneSet = marker_genes_2["pDCs"]
+    TCellGeneSet = np.unique(marker_genes_1["T cells"] + marker_genes_2["T Cells"])
+    CytoTCellGeneSet = marker_genes_2["Cytotoxic T"]
+    memorytcellgeneset = marker_genes_2["Memory T"]
+    cDCgeneset = marker_genes_2["cDCs"]
+
+    plotDifferentialExpression(data, "CTRL4", "B Cells", bCellGeneSet, rank, *ax[0:4])
+    plotDifferentialExpression(
+        data,
+        "CTRL4",
+        "Naive B Cells",
+        NaiveBCellGeneSet,
+        rank,
+        *ax[4:8],
+        ct2=True,
+    )
+    plotDifferentialExpression(data, "CTRL4", "NK Cells", NKCellGeneSet, rank, *ax[8:12])
+    plotDifferentialExpression(
+        data, "CTRL4", "DCs", DCCellGeneSet, rank, *ax[12:16]
+    )
+    plotDifferentialExpression(
+        data, "CTRL4", "pDCs", pDCGeneSet, rank, *ax[16:20], ct2=True,
+    )
+    plotDifferentialExpression(data, "CTRL4", "T Cells", TCellGeneSet, rank, *ax[20:24])
+    plotDifferentialExpression(
+        data, "CTRL4", "Cytotoxic T Cells", CytoTCellGeneSet, rank, *ax[24:28], ct2=True
+    )
+    plotDifferentialExpression(
+        data, "CTRL4", "Memory T Cells", memorytcellgeneset, rank, *ax[28:32], ct2=True
+    )
+    plotDifferentialExpression(
+        data, "CTRL4", "cDCs", cDCgeneset, rank, *ax[32:36], ct2=True
+    )
+    return f
+
+
+def plotDifferentialExpression(
+    data,
+    condition,
+    cell_type,
+    geneset,
+    rank,
+    *args,
+    ct2=False,
+    override=None
+):
+    sampled_data = None
+    if not ct2:
+        sampled_data = data[
+            (data.obs["Cell Type"] != cell_type) | (data.obs["Condition"] != condition)
+        ]
+    else:
+        sampled_data = data[
+            (data.obs["Cell Type2"] != cell_type) | (data.obs["Condition"] != condition)
+        ]
 
     origX = pf2(data, rank, doEmbedding=False)
 
     sampledX = pf2(sampled_data, rank, doEmbedding=False)
 
-    plotGeneFactorsIsolated(sampledX, ax[0], bCellGeneSet)
+    isolated_orig_vals = plotGeneFactorsIsolated(origX, args[0], geneset)
+    isolated_vals = plotGeneFactorsIsolated(sampledX, args[1], geneset)
+    most_exp_cmp = -1
+    most_exp = -1
+    most_exp_cmp2 = -1
+    most_exp2 = -1
 
+    if override is None:
+        for i in range(isolated_orig_vals.shape[1]):
+            exp_sum = np.sum(np.abs(isolated_orig_vals[:, i]))
+            if exp_sum > most_exp:
+                most_exp = exp_sum
+                most_exp_cmp = i
+        for i in range(isolated_vals.shape[1]):
+            exp_sum = np.sum(np.abs(isolated_vals[:, i]))
+            if exp_sum > most_exp2:
+                most_exp2 = exp_sum
+                most_exp_cmp2 = i
+    else:
+        most_exp_cmp = override[0]
+        most_exp_cmp2 = override[1]
     X = np.array(origX.uns["Pf2_A"])
-    X = X[:, 9]
+    X = X[:, most_exp_cmp]
     Y = np.array(sampledX.uns["Pf2_A"])
-    Y = Y[:, 9]
+    Y = Y[:, most_exp_cmp2]
 
     yt = pd.Series(np.unique(origX.obs["Condition"]))
     yt2 = pd.Series(np.unique(sampledX.obs["Condition"]))
 
     assert yt.equals(yt2)
+    if not ct2:
+        print(f"Number of {cell_type}: {len(data[data.obs['Cell Type'] == cell_type])}")
+        print(f"Number of {cell_type} in {condition}: {len(data[(data.obs['Condition'] == condition) & (data.obs['Cell Type'] == cell_type)])}")
+    else:
+        print(f"Number of {cell_type}: {len(data[data.obs['Cell Type2'] == cell_type])}")
+        print(f"Number of {cell_type} in {condition}: {len(data[(data.obs['Condition'] == condition) & (data.obs['Cell Type2'] == cell_type)])}")
 
-    ax[1].scatter(X, Y, s=1)
+    args[2].scatter(X, Y, s=1)
+    numberOfCellType = []
     for i, txt in enumerate(yt):
-        ax[1].annotate(txt, (X[i], Y[i]), fontsize=8)
+        if not ct2:
+            numberOfCellType.append(
+                len(
+                    data[
+                        (data.obs["Condition"] == txt)
+                        & (data.obs["Cell Type"] == cell_type)
+                    ]
+                )
+            )
+        else:
+            numberOfCellType.append(
+                len(
+                    data[
+                        (data.obs["Condition"] == txt)
+                        & (data.obs["Cell Type2"] == cell_type)
+                    ]
+                )
+            )
+        args[2].annotate(txt, (X[i], Y[i]), fontsize=8)
 
-    ax[1].set_xlabel("Original Full Data")
-    ax[1].set_ylabel("Sampled Data With B Cells Removed From CTRL4")
+    args[2].set_xlabel("Original Full Data")
+    args[2].set_ylabel(f"Sampled Data With {cell_type} Removed From {condition}")
 
-    return f
+    args[3].set_xlabel(f"Component {most_exp_cmp + 1} in Original Data Weight")
+    args[3].set_ylabel(f"Number of {cell_type} per condition in Original Data")
+    args[3].scatter(X, numberOfCellType)
+    args[3].set_ylim(bottom=0)
+    args[3].set_xlim(left=0)
 
 
 def plotGeneFactorsIsolated(data, ax, geneset, trim=True):
@@ -92,3 +199,4 @@ def plotGeneFactorsIsolated(data, ax, geneset, trim=True):
         vmax=1,
     )
     ax.set_title("Components by Gene")
+    return X
