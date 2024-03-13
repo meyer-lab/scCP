a898fbd2e84afe693af0ab63d0342a20842f605b
Nathaniel Thomas <catchnate@gmail.com>
2024-03-13 13:02:18 -0700

    Make figures much smaller

--- sccp/figures/figureThomsonDropPercents.py
+++ sccp/figures/figureThomsonDropPercents.py
@@ -6,14 +6,14 @@ from .common import getSetup
 from ..imports import import_thomson
 from ..imports import import_thomson
 import numpy as np
-from .commonFuncs.plotFactors import reorder_table
 from ..factorization import pf2
 import pandas as pd
+
 
 def makeFigure():
     rank = 20
     data = import_thomson()
-    ax, f = getSetup((20, 10), (1 , 1))
+    ax, f = getSetup((3, 3), (1, 1))
     bCellGeneSet = [
         "PXK",
         "MS4A1",
@@ -27,18 +27,33 @@ def makeFigure():
         "VPREB3",
     ]
 
-    plot_weights_across_percents(data, "B Cells", "CTRL4", 0, 1, 0.1, rank, bCellGeneSet, ax[0])
+    plot_weights_across_percents(
+        data, "B Cells", "CTRL4", 0, 1, 0.1, rank, bCellGeneSet, ax[0]
+    )
 
     return f
 
-def plot_weights_across_percents(data, cell_type, condition, percent_min, percent_max, percent_step, rank, geneset, ax):
+
+def plot_weights_across_percents(
+    data,
+    cell_type,
+    condition,
+    percent_min,
+    percent_max,
+    percent_step,
+    rank,
+    geneset,
+    ax,
+):
     """
     Plots the raw weight of the identifying component across the percentages dropped
     """
     vals = {}
     for percent in np.arange(percent_min, percent_max, percent_step):
         print(f"Percent: {percent}")
-        idx = (data.obs["Cell Type"] != cell_type) | (data.obs["Condition"] != condition)
+        idx = (data.obs["Cell Type"] != cell_type) | (
+            data.obs["Condition"] != condition
+        )
         false_idx = idx.index[idx == False]
         idx[
             np.random.choice(
@@ -48,10 +63,13 @@ def plot_weights_across_percents(data, cell_type, condition, percent_min, percen
         sampled_data = data[idx]
 
         sampledX = pf2(sampled_data, rank, doEmbedding=False)
-        gene_values = np.array(sampled_data.varm["Pf2_C"])
+        gene_values = np.array(sampledX.varm["Pf2_C"])
         yt = data.var.index.values
-        ind = reorder_table(gene_values)
-        ind = [ii for ii in ind if yt[ii] in geneset]
+        inds = []
+        for i, gene in enumerate(yt):
+            if gene in geneset:
+                inds.append(i)
+        gene_values = gene_values[inds]
         most_exp_cmp = np.argmax(np.sum(np.abs(gene_values), axis=0))
         Y = np.array(sampledX.uns["Pf2_A"])
         Y = Y[:, most_exp_cmp]
@@ -59,10 +77,18 @@ def plot_weights_across_percents(data, cell_type, condition, percent_min, percen
         for i, txt in enumerate(labels):
             if txt == condition:
                 vals[percent] = Y[i]
+                break
 
-    ax.xlabel(f"Percent of {cell_type} Removed")
-    ax.ylabel("Weight of Identifying Component")
-
-    percents = list(vals.keys())
-    weights = list(vals.values())
-    ax.bar(percents, weights, color ='maroon')
+    percents = []
+    weights = []
+    for percent, weight in vals.items():
+        percents.append(percent)
+        weights.append(weight)
+    weights = [x for _, x in sorted(zip(percents, weights))]
+    percents = sorted(percents)
+    ax.plot(percents, weights)
+    min_val, max_val = min(weights), max(weights)
+    padding = (max_val - min_val) * 0.1
+    ax.set_ylim(min_val - padding, max_val + padding)
+    ax.set_xlabel("Percent of B Cells Dropped")
+    ax.set_ylabel(f"Weight of Identifying Component (Component {most_exp_cmp})")
