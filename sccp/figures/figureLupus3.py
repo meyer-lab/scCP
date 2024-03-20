bff0fe462806aba657a201e820a3402dab2aaaf1
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-03-20 16:10:14 -0700

    Made graphs symmetrical for Lupus3

--- sccp/figures/figureLupus3.py
+++ sccp/figures/figureLupus3.py
@@ -9,15 +9,16 @@ from .commonFuncs.plotUMAP import plotCmpPerCellType, plotCmpUMAP
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((15, 15), (3, 4))
+    ax, f = getSetup((10, 10), (4, 4))
 
     # Add subplot labels
     subplotLabel(ax)
 
     X = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad", backed="r")
 
-    comps = [4, 7, 8, 10, 22, 24]
+    comps = [3, 4, 14, 22, 27, 28]
     for i, cmp in enumerate(comps):
         plotCmpPerCellType(X, cmp, ax[(2 * i)])
         plotCmpUMAP(X, cmp, ax[(2 * i) + 1], cbarMax=0.3)
+        
     return f
