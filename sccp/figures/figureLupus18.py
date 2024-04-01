2a71a43c896c3764886954b88d8f7f501069df94
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-04-01 15:43:55 -0700

    Include myeloid score

--- sccp/figures/figureLupus18.py
+++ sccp/figures/figureLupus18.py
@@ -16,8 +16,7 @@ from ..imports import import_lupus
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((10, 6), (2, 3))
-
+    ax, f = getSetup((21, 4), (1, 6))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -27,13 +26,11 @@ def makeFigure():
     X = cytotoxic_score(X)
     plotScore(X, ax[0], cellType="Cell Type2")
     
-    # X = interferon_score(X)
-    # plotScore(X, ax[1], cellType="Cell Type2")
+    X = interferon_score(X)
+    plotScore(X, ax[1], cellType="Cell Type2")
     
-    # X = interferon_score(X, csv=False)
-    # plotScore(X, ax[2], cellType="Cell Type2")
-
-
+    X = myeloid_score(X)
+    plotScore(X, ax[2], cellType="Cell Type2")
 
     # XX = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad")
     # X = import_lupus(); change gene threshold.001
@@ -52,24 +49,33 @@ def makeFigure():
 
 
 def cytotoxic_score(X):
-    """Scanpy avergage gene score for all cells"""
+    """Scanpy average gene score for all cells"""
     cytotoxic_genes = ["PRF1", "GZMH", "GZMB"]
     X = sc.tl.score_genes(adata=X, gene_list=cytotoxic_genes, copy=True, use_raw=False)
 
     return X
 
 
-def interferon_score(X, csv=True):
-    """Scanpy avergage gene score for all cells"""
-    if csv is True:
-        # Based on DEG analysis for general cell types
-        df = pd.read_csv("sccp/data/Lupus/DEgenes.csv")
-        df = pd.DataFrame(data=df.iloc[:, [0, -1]].to_numpy(), columns = ["Gene", "Category"])
-        df = df.loc[df["Category"] == "Up-Monocyte"]
-        interferon_genes = df["Gene"].to_numpy()
-    else:
-        # Obtained from the figure co-expression figure
-        interferon_genes = ["ISG15", "RGS1", "EPSTI1", "CD8A", "IFI16", "STAT1", "HBB", "SP110", "SP100", "EIF2AK2", "IFI27", "IFI44", "IFI44L", "PATL2", "RNF213", "IFI6", "ISG20", "LAG3", "MX1", "XAF1", "BST2", "OAS1"]
+def myeloid_score(X):  
+    """Scanpy average gene score for all cells"""
+    # Based on DEG analysis for general cell types
+    df = pd.read_csv("sccp/data/Lupus/DEgenes.csv")
+    df = pd.DataFrame(data=df.iloc[:, [0, -1]].to_numpy(), columns = ["Gene", "Category"])
+    df = df.loc[df["Category"] == "Up-Monocyte"]
+    myeloid_genes = df["Gene"].to_numpy()
+    
+    X = sc.tl.score_genes(adata=X, gene_list=myeloid_genes, copy=True, use_raw=False)
+
+    return X
+        
+        
+def interferon_score(X):   
+    """Scanpy average gene score for all cells"""
+    # Obtained from the figure co-expression figure
+    interferon_genes = ["ISG15", "RGS1", "EPSTI1", "CD8A", "IFI16", "STAT1", 
+                        "HBB", "SP110", "SP100", "EIF2AK2", "IFI27", "IFI44", 
+                        "IFI44L", "PATL2", "RNF213", "IFI6", "ISG20", "LAG3", 
+                        "MX1", "XAF1", "BST2", "OAS1"]
         
     X = sc.tl.score_genes(adata=X, gene_list=interferon_genes, copy=True, use_raw=False)
 
@@ -88,7 +94,9 @@ def plotScore(X, ax, cellType="Cell Type"):
         x="Cell Type",
         y="Score",
         hue="Status",
+        order=np.unique(df["Cell Type"]),
         ax=ax,
         showfliers=False)
     
+    ax.set_xticks(ax.get_xticks())
     ax.set_xticklabels(labels=ax.get_xticklabels(), rotation=90)
