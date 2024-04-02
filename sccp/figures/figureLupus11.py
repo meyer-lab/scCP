8eeab7957d1615754ad86e09704eec7b5a337a8d
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-04-02 11:50:41 -0700

    Gene ontology on a per component basis

--- sccp/figures/figureLupus11.py
+++ sccp/figures/figureLupus11.py
@@ -7,7 +7,7 @@ from .common import (
     getSetup,
 )
 from ..geneontology import geneOntology
-
+from gseapy import barplot, dotplot
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
@@ -17,8 +17,52 @@ def makeFigure():
     # Add subplot labels
     subplotLabel(ax)
 
+
     X = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad", backed="r")
 
-    geneOntology(X, 4)
+
+
+
+
+
+
+
+
+
+
+
+
+    df = geneOntology(X, 27)
+
+
+
+
+    # a = dotplot(df, column="FDR q-val", ax=ax[0], ofname=ax[0])
+    a = barplot(df, x="Gene_set", column="FDR q-val", ax=ax[0], ofname="save.png")
+    # a = dotplot(df.res2d, ax=ax[0], ofname="save.png")
+    # a.imshow()
+    f.show()
+    
+
+
+
+
+
+
+
+
+
+
+    # geneSets = [
+    #     "GO_Biological_Process_2021",
+    #     "GO_Cellular_Component_2021",
+    #     "GO_Molecular_Function_2021",
+    # ]
+
+
+
+
+
+
 
     return f
--- sccp/geneontology.py
+++ sccp/geneontology.py
@@ -9,13 +9,34 @@ def geneOntology(X: AnnData, cmpNumb: int) -> pd.DataFrame:
     input for function"""
 
     # Specifies enrichment sets to run against
-    geneSets = [
-        "GO_Biological_Process_2021",
-        "GO_Cellular_Component_2021",
+    geneSets =   [
         "GO_Molecular_Function_2021",
     ]
+    # geneSets =   [
+    #     "GO_Cellular_Component_2021",
+    # ]
+    # geneSets = ["MSigDB_Hallmark_2020" ]
+    geneSets = ["KEGG_2021_Human"]
+    # geneSets = [
+    #     "GO_Biological_Process_2021",
+    #     "GO_Cellular_Component_2021",
+    #     "GO_Molecular_Function_2021",
+    # ]
+
 
     geneRank = pd.Series(X.varm["Pf2_C"][:, cmpNumb - 1], index=X.var_names)
+    geneRank = geneRank.sort_values()
+    
+    geneRank = geneRank.index.values[-50:]
+    
+    
+    for i in geneRank:
+        print(i)
+    # a
+    
+    print( geneRank.iloc[-50:])
+
+
 
     df = gp.prerank(
         geneRank,
@@ -25,7 +46,14 @@ def geneOntology(X: AnnData, cmpNumb: int) -> pd.DataFrame:
         verbose=True,
         threads=30,
     ).res2d
-
-    df = df.loc[df["FWER p-val"] < 0.05]
+    
+    
+    
+    # df = gp.enrichr(gene_list=geneRank.index.tolist(),
+    #              gene_sets=geneSets,
+    #             organism="Human",
+    #              outdir=None)
+    
+    # df = df.loc[df["FWER p-val"] < 0.05]
 
     return df
