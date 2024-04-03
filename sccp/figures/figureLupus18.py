a49c3da87d14ae4fa6fef935f6d4da7eea4f48cb
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-04-03 16:59:53 -0700

    Shift values

--- sccp/figures/figureLupus18.py
+++ sccp/figures/figureLupus18.py
@@ -27,17 +27,26 @@ def makeFigure():
     total_df = pd.DataFrame([])
 
     cmpWeights = np.concatenate(([X.varm["Pf2_C"][:, comps[0]-1]], [X.varm["Pf2_C"][:, comps[1]-1]]))
+    
+    
+    cmpWeights[0, :] -= np.min(cmpWeights[0, :]) 
+    cmpWeights[1, :] -= np.min(cmpWeights[1, :]) 
 
     df = pd.DataFrame(data=cmpWeights.transpose(), index=X.var_names, columns=["Component 22", "Component 28"])
 
     sns.scatterplot(data=df, x="Component 22", y="Component 28", ax=ax[0])
+    
+    
+    # df = pd.DataFrame(data=cmpWeights.transpose()), index=X.var_names, columns=["Component 22", "Component 28"])
 
     sns.scatterplot(data=df, x="Component 22", y="Component 28", ax=ax[1])
-    # ax[1].set(xscale="symlog", yscale="symlog", xlim=[-.5, .1])
+    ax[1].set(xscale="log", yscale="log")
     # ax[1].set_yticks([.1 , .01, .001, 0, -.001, .01, .1])
     
-    ax[1].set(xlim=[-.5, .5],yscale="symlog",xscale="symlog" )
-    ax[1].set_yticks([1, .1 , .01, .001, 0, -.001, .01, .1])
+    
+    # ax[1].ticklabel_format(axis='both', style='sci', scilimits=(0,0))
+    # ax[1].set(xlim=[-1, 1],yscale="symlog",xscale="symlog" )
+    # ax[1].set_yticks([1, .1 , .01, .001, 0, -.001, .01, .1])
 
 
 
