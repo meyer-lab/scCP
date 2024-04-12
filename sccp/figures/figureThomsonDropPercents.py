e703847ae29c3c8a18f59895346d664defa248f9
Nathaniel Thomas <55164518+Nathaniel-github@users.noreply.github.com>
2024-04-12 07:30:52 -0700

    Odd error with numpy (#332)
    * Odd error with numpy
    
    * update title of graph
    
    * Was a poetry issue all fixed. Displays factor plots for all cell types being dropped in CTRL4
    
    * Currently plotting is still bugging but need to update branch to confirm
    
    * Commit before merge
    
    * B Cell drop investigate genes
    
    * Isolating component 10
    
    * Conclusively show PARAFAC2's ability to detect and project changes within a larger dataset
    
    * Clean version of this branch to show demonstrate how PARAFAC2 reacts to changes
    
    * Finished ThomsonDrop figure with all cell type subpopulations
    
    * Updates with suggestions
    
    * update gitignore to not push nohup.out file
    
    * Resolve comments
    
    * remove nohup
    
    * Add docstrings to the methods
    
    * Working on changes to picking out cell type identifying component
    
    * Make figures much smaller
    
    * Finished drop and dropPercents figures
    
    * Finished adding comments
    
    * Make changes based on comments
    
    * Make final change based on comments
    
    * Rename variables for clarity
    
    * Update poetry
    
    * Change override to pass test
    
    * Fix signed int build error
    
    * Remove commented lines
    
    * Try casting to fix build issue
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

---
+++ sccp/figures/figureThomsonDropPercents.py
@@ -0,0 +1,85 @@
+"""
+Plots the differences in the identifying component weight with different percentages of a cell type removed from the data. The determining component is the component that has the highest correlation with the number of cells in the cell type selected.
+"""
+
+from .common import getSetup
+from ..imports import import_thomson
+import numpy as np
+from ..factorization import pf2
+import pandas as pd
+import seaborn as sns
+
+
+def makeFigure():
+    rank = 20
+    data = import_thomson()
+    ax, f = getSetup((2, 2), (1, 1))
+    bCellGeneSet = [
+        "PXK",
+        "MS4A1",
+        "CD19",
+        "CD74",
+        "CD79A",
+        "CD79B",
+        "BANK1",
+        "PTPRC",
+        "CR2",
+        "VPREB3",
+    ]
+
+    plot_weights_across_percents(
+        data, "B Cells", "CTRL4", 0, 1, 0.1, rank, bCellGeneSet, ax[0]
+    )
+
+    ### Can add other cell types here
+
+    return f
+
+
+def plot_weights_across_percents(
+    data,
+    cell_type,
+    condition,
+    percent_min,
+    percent_max,
+    percent_step,
+    rank,
+    geneset,
+    ax,
+):
+    """
+    Plots the raw weight of the identifying component across the percentages dropped
+    """
+    all_percents, all_weights = [], []
+    for i in range(5):
+        vals = {}
+        for percent in np.arange(percent_min, percent_max, percent_step):
+            idx = (data.obs["Cell Type"] != cell_type) | (
+                data.obs["Condition"] != condition
+            )
+
+            # Calculate the number of cells to drop based on the percentage
+            false_idx = idx[~idx].index
+            size = int(len(false_idx) * (1 - percent)) 
+
+            idx[np.random.choice(false_idx, size=size, replace=False)] = True
+            sampled_data = data[idx]
+
+            sampledX = pf2(sampled_data, rank, doEmbedding=False)
+            gene_values = np.array(sampledX.varm["Pf2_C"])[
+                [i for i, gene in enumerate(data.var.index.values) if gene in geneset]
+            ] # Sum of the gene expression for the marker genes
+            most_exp_cmp = np.argmax(np.sum(np.abs(gene_values), axis=0)) # The component with the highest sum of gene expression
+            Y = np.array(sampledX.uns["Pf2_A"])[:, most_exp_cmp]
+            idx = np.where(np.unique(sampledX.obs["Condition"]) == condition)[0][0]  # Aims to find the index of the condition to set its corresponding weight
+            vals[percent] = Y[idx]
+
+        percents, weights = zip(*sorted((int(p * 100), w) for p, w in vals.items()))
+        all_percents += percents
+        all_weights += weights
+
+    df = pd.DataFrame(data={"Percent": all_percents, "Weight": all_weights})
+    df = df.sort_values(by="Percent")
+    sns.lineplot(data=df, x="Percent", y="Weight", ax=ax)
+    ax.set_xlabel("% B Cells Dropped")
+    ax.set_ylabel(f"Component {most_exp_cmp} Weight")
