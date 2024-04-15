031cc39d2ceca2c7a5ae0d2e1f67290bcc0bff34
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-04-15 16:29:27 -0700

    Use old lableing for scores

---
+++ sccp/figures/figureLupus20.py
@@ -0,0 +1,107 @@
+"""
+Lupus
+"""
+from anndata import read_h5ad
+from .common import (
+    subplotLabel,
+    getSetup,
+)
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from scipy.stats import linregress
+import scanpy as sc
+from ..imports import import_lupus
+from ..gating import getHiResOldLupus
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((21, 4), (1, 6))
+
+    # Add subplot labels
+    subplotLabel(ax)
+    
+    X = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad")
+    X = getHiResOldLupus(X)
+    
+    X = cytotoxic_score(X)
+    plotScore(X, ax[0], cellType="Cell Type Old2")
+    ax[0].set(title="Cytotoxic Score")
+    
+    X = interferon_score(X)
+    plotScore(X, ax[1], cellType="Cell Type Old2")
+    ax[1].set(title="IFN Score")
+    
+    X = myeloid_score(X)
+    plotScore(X, ax[2], cellType="Cell Type Old2")
+    ax[2].set(title="Myeloid Score")
+
+    # XX = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad")
+    # X = import_lupus(geneThreshold=.001)
+    # X.obs["Cell Type2"] = XX.obs["Cell Type2"]
+    # X = cytotoxic_score(X)
+    # plotScore(X, ax[3], cellType="Cell Type2")
+    
+    # X = interferon_score(X)
+    # plotScore(X, ax[4], cellType="Cell Type2")
+    
+    # X = myeloid_score(X)
+    # plotScore(X, ax[5], cellType="Cell Type2")
+    
+    
+    return f
+
+
+def cytotoxic_score(X):
+    """Scanpy average gene score for all cells"""
+    cytotoxic_genes = ["PRF1", "GZMH", "GZMB"]
+    X = sc.tl.score_genes(adata=X, gene_list=cytotoxic_genes, copy=True, use_raw=False)
+
+    return X
+
+
+def myeloid_score(X):  
+    """Scanpy average gene score for all cells"""
+    # Based on DEG analysis for general cell types
+    df = pd.read_csv("sccp/data/Lupus/DEgenes.csv")
+    df = pd.DataFrame(data=df.iloc[:, [0, -1]].to_numpy(), columns = ["Gene", "Category"])
+    df = df.loc[df["Category"] == "Up-Monocyte"]
+    myeloid_genes = df["Gene"].to_numpy()
+    
+    X = sc.tl.score_genes(adata=X, gene_list=myeloid_genes, copy=True, use_raw=False)
+
+    return X
+        
+        
+def interferon_score(X):   
+    """Scanpy average gene score for all cells"""
+    # Obtained from the figure co-expression figure
+    interferon_genes = ["ISG15", "RGS1", "EPSTI1", "CD8A", "IFI16", "STAT1", 
+                        "HBB", "SP110", "SP100", "EIF2AK2", "IFI27", "IFI44", 
+                        "IFI44L", "PATL2", "RNF213", "IFI6", "ISG20", "LAG3", 
+                        "MX1", "XAF1", "BST2", "OAS1"]
+        
+    X = sc.tl.score_genes(adata=X, gene_list=interferon_genes, copy=True, use_raw=False)
+
+    return X
+
+def plotScore(X, ax, cellType="Cell Type"):
+    """Plots average score  across cell types and patient status"""
+    df = pd.DataFrame({"Score": X.obs["score"].values})
+    df["Status"] = X.obs["SLE_status"].values
+    df["Condition"] = X.obs["Condition"].values
+    df["Cell Type"] = X.obs[cellType].values
+    df = df.groupby(["Status", "Cell Type", "Condition"], observed=False).mean().reset_index()
+    
+    sns.boxplot(
+        data=df,
+        x="Cell Type",
+        y="Score",
+        hue="Status",
+        order=np.unique(df["Cell Type"]),
+        ax=ax,
+        showfliers=False)
+    
+    ax.set_xticks(ax.get_xticks())
+    ax.set_xticklabels(labels=ax.get_xticklabels(), rotation=90)
