59f1c8b881b468baf718c9e58062a6b7661dd5db
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-04-19 09:15:54 -0700

    Alter to include cell count as well

--- sccp/figures/figureThomson7.py
+++ sccp/figures/figureThomson7.py
@@ -10,7 +10,7 @@ from .common import subplotLabel, getSetup
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((11, 14), (4, 2))
+    ax, f = getSetup((12, 18), (8, 2))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -18,7 +18,7 @@ def makeFigure():
     X = read_h5ad("/opt/pf2/thomson_fitted.h5ad", backed="r")
 
     df = pd.DataFrame(
-        {"Cell Type": X.obs["Cell Type"], "Condition": X.obs["Condition"]}
+        {"Cell Type": X.obs["Cell Type2"], "Condition": X.obs["Condition"]}
     )
 
     # Per condition counts
@@ -26,33 +26,37 @@ def makeFigure():
         df.groupby(["Condition"], observed=True).size().reset_index(name="Cell Number")
     )
     
-    sns.barplot(data=dfCond, x="Condition", y="Cell Number", color="k", ax=ax[0])
-    ax[0].set(ylabel="Number of Cells")
-    rotate_axis(ax[0])
+    # sns.barplot(data=dfCond, x="Condition", y="Cell Number", color="k", ax=ax[0])
+    # ax[0].set(ylabel="Number of Cells")
+    # rotate_axis(ax[0])
 
 
     # Per condition cell type percentages
     dfCellType = (
         df.groupby(["Cell Type", "Condition"], observed=True)
         .size()
-        .reset_index(name="Count")
+        .reset_index(name="Cell Count")
     )
-    dfCellType["Cell Type Percentage"] = (
-        100
-        * dfCellType["Count"]
-        / dfCellType.groupby("Condition", observed=True)["Count"].transform("sum")
-    )
+    # dfCellType["Cell Type Percentage"] = (
+    #     100
+    #     * dfCellType["Count"]
+    #     / dfCellType.groupby("Condition", observed=True)["Count"].transform("sum")
+    # )
 
     for i, (name, group) in enumerate(dfCellType.groupby("Cell Type", observed=True)):
         sns.barplot(
             data=group,
             x="Condition", 
-            y="Cell Type Percentage",
+            y="Cell Count",
             color="k",
-            ax=ax[i+1],
+            ax=ax[i],
         )
-        rotate_axis(ax[i+1])
-        ax[i+1].set(ylabel=f"{name} Percentage")
+        rotate_axis(ax[i])
+        ax[i].set(ylabel=f"{name} Count")
+        
+    f.delaxes(ax[13])   
+    f.delaxes(ax[14]) 
+    f.delaxes(ax[15])     
        
 
 
