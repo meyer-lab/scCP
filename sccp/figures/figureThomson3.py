5b3111a47a6869175e7d9eec8c466729f6b61a7e
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2024-04-22 15:18:20 -0700

    Cleanup size and structure of figures (#422)
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figureThomson3.py
+++ sccp/figures/figureThomson3.py
@@ -12,14 +12,14 @@ from .commonFuncs.plotUMAP import (
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((10, 8), (3, 3))
+    ax, f = getSetup((12, 10), (5, 4))
 
     # Add subplot labels
     subplotLabel(ax)
 
     X = read_h5ad("/opt/pf2/thomson_fitted.h5ad", backed="r")
 
-    for i in range(0, 8):
-        plotCmpUMAP(X, i, ax[i])
+    for i in range(1, 21):
+        plotCmpUMAP(X, i, ax[i-1], cbarMax=.3)
 
     return f
