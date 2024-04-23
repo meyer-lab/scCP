348b6ea7d8c8357fd56044b3e81d008369009e59
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-04-22 20:22:44 -0700

    Make one general function

--- sccp/figures/figureThomson7.py
+++ sccp/figures/figureThomson7.py
@@ -6,6 +6,7 @@ from anndata import read_h5ad
 import pandas as pd
 import seaborn as sns
 from .common import subplotLabel, getSetup
+from .commonFuncs.plotGeneral import cell_count_perc_df, rotate_xaxis
 
 
 def makeFigure():
@@ -17,33 +18,11 @@ def makeFigure():
     subplotLabel(ax)
 
     X = read_h5ad("/opt/pf2/thomson_fitted.h5ad", backed="r")
-
-    df = pd.DataFrame(
-        {"Cell Type": X.obs["Cell Type"], "Condition": X.obs["Condition"]}
-    )
-
-    # Per condition counts
-    dfCond = (
-        df.groupby(["Condition"], observed=True).size().reset_index(name="Cell Number")
-    )
-
-    sns.barplot(data=dfCond, x="Condition", y="Cell Number", color="k", ax=ax[0])
-    ax[0].set(ylabel="Number of Cells")
-    rotate_axis(ax[0])
-
-    # Per condition cell type percentages
-    dfCellType = (
-        df.groupby(["Cell Type", "Condition"], observed=True)
-        .size()
-        .reset_index(name="Count")
-    )
-    dfCellType["Cell Type Percentage"] = (
-        100
-        * dfCellType["Count"]
-        / dfCellType.groupby("Condition", observed=True)["Count"].transform("sum")
-    )
-
-    for i, (name, group) in enumerate(dfCellType.groupby("Cell Type", observed=True)):
+    
+    plot_cell_count(X, ax[0])
+    
+    df = cell_count_perc_df(X, celltype="Cell Type")
+    for i, (name, group) in enumerate(df.groupby("Cell Type", observed=True)):
         sns.barplot(
             data=group,
             x="Condition",
@@ -51,12 +30,19 @@ def makeFigure():
             color="k",
             ax=ax[i + 1],
         )
-        rotate_axis(ax[i + 1])
+        rotate_xaxis(ax[i + 1])
         ax[i + 1].set(ylabel=f"{name} Percentage")
 
     return f
 
 
-def rotate_axis(ax):
-    ax.set_xticks(ax.get_xticks())
-    ax.set_xticklabels(labels=ax.get_xticklabels(), rotation=90)
+def plot_cell_count(X, ax):
+    """Plots overall cell count for Chen et al."""
+    df = X.obs[["Condition"]].reset_index(drop=True)
+    dfCond = df.groupby(["Condition"], observed=True).size().reset_index(name="Cell Count")
+
+    sns.barplot(data=dfCond, x="Condition", y="Cell Count", color="k", ax=ax)
+    rotate_xaxis(ax)
+
+
+    return 
