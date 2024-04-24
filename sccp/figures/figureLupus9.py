d60205eee13c52de0d76fd4d9fa5719c6e762e44
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-04-24 09:11:40 -0700

    Cleanup pair condition factors plottign

--- sccp/figures/figureLupus9.py
+++ sccp/figures/figureLupus9.py
@@ -14,29 +14,32 @@ from ..factorization import correct_conditions
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((6, 8), (1, 2))
+    ax, f = getSetup((5, 3), (1, 2))
 
     # Add subplot labels
     subplotLabel(ax)
 
     X = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad")
+    
+    
+    plot_pair_cond_factors(X, cmp1=27, cmp2=28, ax=ax[0])
+    plot_pair_cond_factors(X, cmp1=14, cmp2=4, ax=ax[1])
 
-    predict = "SLE_status"
+
+    return f
+
+
+
+def plot_pair_cond_factors(X, cmp1, cmp2, ax):
+    """Plots two condition components weights"""
+    cond_factors = correct_conditions(X)
     condStatus = samples_only_lupus(X)
     condStatus = condStatus.set_index("Condition")
 
-    df = pd.DataFrame(
-        correct_conditions(X),
-        columns=[f"Cmp. {i}" for i in np.arange(1, X.uns["Pf2_A"].shape[1] + 1)],
-        index=condStatus.index,
-    )
+    cmpWeights = np.concatenate(([cond_factors[:, cmp1-1]], [cond_factors[:, cmp2-1]]))
+    df = pd.DataFrame(data=cmpWeights.transpose(), columns=[f"Cmp. {cmp1}", f"Cmp. {cmp2}"],
+                       index=condStatus.index)
+
     df = df.merge(condStatus, left_index=True, right_index=True)
 
-    twoCmp = [[13, 21], [13, 27]]
-
-    for i, pair in enumerate(twoCmp):
-        sns.scatterplot(
-            data=df, x=f"Cmp. {pair[0]}", y=f"Cmp. {pair[1]}", hue=predict, ax=ax[i]
-        )
-
-    return f
+    sns.scatterplot(data=df, x=f"Cmp. {cmp1}", y=f"Cmp. {cmp2}", hue="SLE_status", ax=ax)
