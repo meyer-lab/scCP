4b724056b2f50bce64a45a9aaaa7e22f868051a0
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2024-05-08 15:51:33 -0700

    Clean up logisitic regression make 4th batch training (#446)
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figureLupus4.py
+++ sccp/figures/figureLupus4.py
@@ -19,21 +19,14 @@ def makeFigure():
 
     X = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad", backed="r")
 
-    # ranks = np.arange(5, 10, 5)
     ranks=[1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
     # plot_r2x(X, ranks, ax[0])
 
-    
-    
-    plot_predaccuracy_ranks_lupus(X, ranks, ax[1], error_metric="roc_auc")
+    # plot_predaccuracy_ranks_lupus(X, ranks, ax[1], error_metric="roc_auc")
     ax[1].set(
         xticks=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
-    
     X = [0, 50]
     Y = [.84, .84]
- 
-    # Plot a graph
     ax[1].plot(X, Y, linestyle='--')
-
 
     return f
