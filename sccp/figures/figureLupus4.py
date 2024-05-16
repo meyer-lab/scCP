f60ef0afb97818f7d2171966895c412e670bb29c
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2024-05-16 08:43:04 -0700

    Save changes to logreg (#448)
    * Save changes to logreg
    
    * Fix ruff format suggestions
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figureLupus4.py
+++ sccp/figures/figureLupus4.py
@@ -1,12 +1,11 @@
 """
-Lupus: R2X for PCA/Pf2 and prediction accuracy for different components
+Lupus: R2X for PCA/Pf2 and accuracy for different components
 """
 
-import numpy as np
 from anndata import read_h5ad
 from .common import subplotLabel, getSetup
-from .commonFuncs.plotGeneral import plot_r2x
-from .commonFuncs.plotLupus import plot_predaccuracy_ranks_lupus
+# from .commonFuncs.plotGeneral import plot_r2x
+# from .commonFuncs.plotLupus import plot_accuracy_ranks_lupus
 
 
 def makeFigure():
@@ -19,14 +18,13 @@ def makeFigure():
 
     X = read_h5ad("/opt/andrew/lupus/lupus_fitted_ann.h5ad", backed="r")
 
-    ranks=[1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    # ranks=[1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
     # plot_r2x(X, ranks, ax[0])
 
-    # plot_predaccuracy_ranks_lupus(X, ranks, ax[1], error_metric="roc_auc")
-    ax[1].set(
-        xticks=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+    # plot_accuracy_ranks_lupus(X, ranks, ax[1], error_metric="roc_auc")
+    ax[1].set(xticks=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
     X = [0, 50]
-    Y = [.84, .84]
-    ax[1].plot(X, Y, linestyle='--')
+    Y = [0.84, 0.84]
+    ax[1].plot(X, Y, linestyle="--")
 
     return f
