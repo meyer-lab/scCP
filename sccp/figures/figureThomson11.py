8cd5fc135e93795683d691eb74aa4875fbcf350c
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-05-21 10:58:33 -0700

    Plot on one graph metrics

--- sccp/figures/figureThomson11.py
+++ sccp/figures/figureThomson11.py
@@ -20,7 +20,7 @@ import scipy.sparse as sps
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((3, 3), (1, 2))
+    ax, f = getSetup((3, 3), (1, 1))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -28,23 +28,27 @@ def makeFigure():
     # Import data
     X = read_h5ad("/opt/pf2/thomson_fitted.h5ad", backed="r")
     
-    
-    
-    # plot_batch_correction(X, "projections", ax[0])
+    # pf2_batch_df = batch_correction_metrics(X, "projections")
+    # pf2_batch_df["Fit"] = "Pf2"
     
     # pc = PCA(n_components=20, svd_solver="arpack")
     # X = X.to_memory()
     # XX = sps.csr_array(X.X)
     # pcaPoints = pc.fit_transform(XX)
-
     # X.obsm["pca"] = pacmap.PaCMAP().fit_transform(pcaPoints)
-    # plot_batch_correction(X, "pca", ax[1])
+    # pca_batch_df = batch_correction_metrics(X, "pca")
+    # pca_batch_df["Fit"] = "PCA"
     
+    # batch_df =  pd.concat([pf2_batch_df, pca_batch_df])
+
+    # sns.barplot(data=batch_df, x="Value", y="Metric", hue="Fit", ax=ax[0])
     
-  
+
     return f
 
-def plot_batch_correction(X, embed, ax):
+
+def batch_correction_metrics(X, embed):
+    """Returns SCIB metrics for batch correction and biological conservation"""
     clisi = scib.me.clisi_graph(X.to_memory(), label_key="Cell Type", type_="embed", use_rep=embed)
     asw = scib.me.isolated_labels_asw(X.to_memory(), batch_key="Condition", label_key="Cell Type", embed=embed)
     silh = scib.me.silhouette(X, label_key="Cell Type", embed=embed)
@@ -56,16 +60,14 @@ def plot_batch_correction(X, embed, ax):
     scib.me.cluster_optimal_resolution(X, cluster_key="leiden", label_key="Cell Type")
     ari = scib.me.ari(X, cluster_key="leiden", label_key="Cell Type")
     nmi = scib.me.nmi(X, cluster_key="leiden", label_key="Cell Type")
-
     
     metric_df = pd.DataFrame([{"SB": sb, "GC": gc, "iLisi": ilisi, "cLisi": clisi, "ASW": asw,
-                               "Silhouette": silh, "ARI": ari, "NMI": nmi}])
+                               "S": silh, "ARI": ari, "NMI": nmi}])
     
     metric_df = metric_df.reset_index(drop=True)
     
     metric_df = pd.melt(metric_df, value_vars=["SB", "GC", "iLisi", "cLisi", "ASW", 
-                                               "ARI", "Silhouette", "NMI"]).rename(
+                                               "ARI", "S", "NMI"]).rename(
         columns={"variable": "Metric", "value": "Value"})
-    
-    sns.barplot(data=metric_df, x="Metric", y="Value", ax=ax)
-    
+                                               
+    return metric_df
