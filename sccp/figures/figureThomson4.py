ea4bd0f49b0fa073e1510a95a5b967fe8a735dfe
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2024-05-22 14:57:56 -0700

    Add scib metrics to Thomson plot (#451)
    * Add scib metrics to Thomson plot
    
    * Changes axes
    
    * update poetry and run ruff
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figureThomson4.py
+++ sccp/figures/figureThomson4.py
@@ -8,6 +8,10 @@ from .common import subplotLabel, getSetup
 from .commonFuncs.plotPaCMAP import plot_gene_pacmap, plot_labels_pacmap
 import pacmap
 import numpy as np
+import scib
+import scanpy as sc
+import pandas as pd
+import seaborn as sns
 
 
 def makeFigure():
@@ -20,30 +24,85 @@ def makeFigure():
 
     X = read_h5ad("/opt/pf2/thomson_fitted.h5ad")
 
-    genes = ["NKG7"]
-    for i, gene in enumerate(genes):
-        plot_gene_pacmap(gene, "Pf2", X, ax[i])
-
     drugs = ["Triamcinolone Acetonide", "Alprostadil", "Budesonide"]
     for i, drug in enumerate(drugs):
-        plot_labels_pacmap(X, "Condition", ax[i + 1], drug, cmap="Set1")
-        ax[i + 1].set(title="Pf2-Based Decomposition")
+        plot_labels_pacmap(X, "Condition", ax[i], drug, cmap="Set1")
+        ax[i].set(title="Pf2-Based Decomposition")
 
-    plot_labels_pacmap(X, "Cell Type", ax[4])
-    plot_labels_pacmap(X, "Cell Type2", ax[5])
+    plot_labels_pacmap(X, "Cell Type", ax[3])
+    plot_labels_pacmap(X, "Cell Type2", ax[4])
 
     # PCA dimension reduction
     pc = PCA(n_components=20)
     pcaPoints = pc.fit_transform(np.asarray(X.X - X.var["means"].values))
     X.obsm["X_pf2_PaCMAP"] = pacmap.PaCMAP().fit_transform(pcaPoints)
 
-    for i, gene in enumerate(genes):
-        plot_gene_pacmap(gene, "PCA", X, ax[i + 6])
+    plot_gene_pacmap("NKG7", "PCA", X, ax[5])
 
     for i, drug in enumerate(drugs):
-        plot_labels_pacmap(X, "Condition", ax[i + 7], drug, cmap="Set1")
-        ax[i + 7].set(title="PCA-Based Decomposition")
+        plot_labels_pacmap(X, "Condition", ax[i + 6], drug, cmap="Set1")
+        ax[i + 6].set(title="PCA-Based Decomposition")
 
-    plot_labels_pacmap(X, "Cell Type", ax[10])
+    plot_labels_pacmap(X, "Cell Type", ax[9])
+
+    pf2_batch_df = batch_correction_metrics(X, "projections")
+    pf2_batch_df["Fit"] = "Pf2"
+
+    X.obsm["pca"] = pcaPoints
+    pca_batch_df = batch_correction_metrics(X, "pca")
+    pca_batch_df["Fit"] = "PCA"
+
+    batch_df = pd.concat([pf2_batch_df, pca_batch_df])
+    sns.barplot(data=batch_df, x="Value", y="Metric", hue="Fit", ax=ax[10])
 
     return f
+
+
+def batch_correction_metrics(X, embed):
+    """Returns SCIB metrics for batch correction and biological conservation"""
+    clisi = scib.me.clisi_graph(
+        X.to_memory(), label_key="Cell Type", type_="embed", use_rep=embed
+    )
+    ils = scib.me.isolated_labels_asw(
+        X.to_memory(), batch_key="Condition", label_key="Cell Type", embed=embed
+    )
+    silh = scib.me.silhouette(X, label_key="Cell Type", embed=embed)
+    sb = scib.me.silhouette_batch(
+        X, batch_key="Condition", label_key="Cell Type", embed=embed
+    )
+    ilisi = scib.me.ilisi_graph(
+        X.to_memory(), batch_key="Condition", type_="embed", use_rep=embed
+    )
+    sc.pp.neighbors(X, use_rep=embed)
+    ilf = scib.me.isolated_labels_f1(
+        X.to_memory(), batch_key="Condition", label_key="Cell Type", embed=embed
+    )
+    gc = scib.me.graph_connectivity(X, label_key="Cell Type")
+    scib.me.cluster_optimal_resolution(X, cluster_key="leiden", label_key="Cell Type")
+    ari = scib.me.ari(X, cluster_key="leiden", label_key="Cell Type")
+    nmi = scib.me.nmi(X, cluster_key="leiden", label_key="Cell Type")
+
+    metric_df = pd.DataFrame(
+        [
+            {
+                "SB": sb,
+                "GC": gc,
+                "iLisi": ilisi,
+                "cLisi": clisi,
+                "ILS": ils,
+                "S": silh,
+                "ARI": ari,
+                "NMI": nmi,
+                "ILF": ilf,
+            }
+        ]
+    )
+
+    metric_df = metric_df.reset_index(drop=True)
+
+    metric_df = pd.melt(
+        metric_df,
+        value_vars=["SB", "GC", "iLisi", "cLisi", "ILS", "ARI", "S", "NMI", "ILF"],
+    ).rename(columns={"variable": "Metric", "value": "Value"})
+
+    return metric_df
