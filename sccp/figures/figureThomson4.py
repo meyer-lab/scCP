f11dcc3635005cfcd9d4bf2cc35dd6448200c0a6
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-05-22 14:20:17 -0700

    update poetry and run ruff

--- sccp/figures/figureThomson4.py
+++ sccp/figures/figureThomson4.py
@@ -44,7 +44,7 @@ def makeFigure():
         ax[i + 6].set(title="PCA-Based Decomposition")
 
     plot_labels_pacmap(X, "Cell Type", ax[9])
-    
+
     pf2_batch_df = batch_correction_metrics(X, "projections")
     pf2_batch_df["Fit"] = "Pf2"
 
@@ -52,34 +52,57 @@ def makeFigure():
     pca_batch_df = batch_correction_metrics(X, "pca")
     pca_batch_df["Fit"] = "PCA"
 
-    batch_df =  pd.concat([pf2_batch_df, pca_batch_df])
+    batch_df = pd.concat([pf2_batch_df, pca_batch_df])
     sns.barplot(data=batch_df, x="Value", y="Metric", hue="Fit", ax=ax[10])
-
 
     return f
 
 
 def batch_correction_metrics(X, embed):
     """Returns SCIB metrics for batch correction and biological conservation"""
-    clisi = scib.me.clisi_graph(X.to_memory(), label_key="Cell Type", type_="embed", use_rep=embed)
-    ils = scib.me.isolated_labels_asw(X.to_memory(), batch_key="Condition", label_key="Cell Type", embed=embed)
+    clisi = scib.me.clisi_graph(
+        X.to_memory(), label_key="Cell Type", type_="embed", use_rep=embed
+    )
+    ils = scib.me.isolated_labels_asw(
+        X.to_memory(), batch_key="Condition", label_key="Cell Type", embed=embed
+    )
     silh = scib.me.silhouette(X, label_key="Cell Type", embed=embed)
-    sb = scib.me.silhouette_batch(X, batch_key="Condition", label_key="Cell Type", embed=embed)
-    ilisi = scib.me.ilisi_graph(X.to_memory(), batch_key="Condition", type_="embed", use_rep=embed)
+    sb = scib.me.silhouette_batch(
+        X, batch_key="Condition", label_key="Cell Type", embed=embed
+    )
+    ilisi = scib.me.ilisi_graph(
+        X.to_memory(), batch_key="Condition", type_="embed", use_rep=embed
+    )
     sc.pp.neighbors(X, use_rep=embed)
-    ilf = scib.me.isolated_labels_f1(X.to_memory(), batch_key="Condition", label_key="Cell Type", embed=embed)
+    ilf = scib.me.isolated_labels_f1(
+        X.to_memory(), batch_key="Condition", label_key="Cell Type", embed=embed
+    )
     gc = scib.me.graph_connectivity(X, label_key="Cell Type")
     scib.me.cluster_optimal_resolution(X, cluster_key="leiden", label_key="Cell Type")
     ari = scib.me.ari(X, cluster_key="leiden", label_key="Cell Type")
     nmi = scib.me.nmi(X, cluster_key="leiden", label_key="Cell Type")
 
-    metric_df = pd.DataFrame([{"SB": sb, "GC": gc, "iLisi": ilisi, "cLisi": clisi, "ILS": ils,
-                               "S": silh, "ARI": ari, "NMI": nmi, "ILF": ilf}])
+    metric_df = pd.DataFrame(
+        [
+            {
+                "SB": sb,
+                "GC": gc,
+                "iLisi": ilisi,
+                "cLisi": clisi,
+                "ILS": ils,
+                "S": silh,
+                "ARI": ari,
+                "NMI": nmi,
+                "ILF": ilf,
+            }
+        ]
+    )
 
     metric_df = metric_df.reset_index(drop=True)
 
-    metric_df = pd.melt(metric_df, value_vars=["SB", "GC", "iLisi", "cLisi", "ILS", 
-                                               "ARI", "S", "NMI", "IL"]).rename(
-        columns={"variable": "Metric", "value": "Value"})
+    metric_df = pd.melt(
+        metric_df,
+        value_vars=["SB", "GC", "iLisi", "cLisi", "ILS", "ARI", "S", "NMI", "ILF"],
+    ).rename(columns={"variable": "Metric", "value": "Value"})
 
     return metric_df
