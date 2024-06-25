c994b4be8114620890e0a0d2998cf505aaf72705
Aaron Meyer <ameyer@ucla.edu>
2024-06-25 16:47:00 -0700

    Update parafac2 version

--- sccp/imports.py
+++ sccp/imports.py
@@ -1,42 +1,7 @@
-import numpy as np
 import pandas as pd
 import anndata
-import scanpy as sc
-from scipy.sparse import spmatrix
-from sklearn.utils.sparsefuncs import inplace_column_scale, mean_variance_axis
+from parafac2.normalize import prepare_dataset
 from .gating import gateThomsonCells
-
-
-def prepare_dataset(
-    X: anndata.AnnData, condition_name: str, geneThreshold: float
-) -> anndata.AnnData:
-    assert isinstance(X.X, spmatrix)
-    assert np.amin(X.X.data) >= 0.0  # type: ignore
-
-    # Filter out genes with too few reads
-    readmean, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    X = X[:, readmean > geneThreshold]
-
-    # Normalize read depth
-    sc.pp.normalize_total(X, exclude_highly_expressed=False, inplace=True)
-
-    # Scale genes by sum
-    readmean, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    readsum = X.shape[0] * readmean
-    inplace_column_scale(X.X, 1.0 / readsum)
-
-    # Transform values
-    X.X.data = np.log10((1000.0 * X.X.data) + 1.0)  # type: ignore
-
-    # Get the indices for subsetting the data
-    _, sgIndex = np.unique(X.obs_vector(condition_name), return_inverse=True)
-    X.obs["condition_unique_idxs"] = sgIndex
-
-    # Pre-calculate gene means
-    means, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    X.var["means"] = means
-
-    return X
 
 
 def import_thomson() -> anndata.AnnData:
