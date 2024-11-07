448f15ab1f575f813949d320bcd0ffc8da552d56
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2024-11-07 15:16:16 -0800

    Add cell type pacmaps (#472)
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- sccp/figures/figureS6.py
+++ sccp/figures/figureS6.py
@@ -5,17 +5,23 @@ Figure S6
 import anndata
 
 from .common import getSetup, subplotLabel
-from .commonFuncs.plotPaCMAP import plot_wp_pacmap
+from .commonFuncs.plotPaCMAP import plot_wp_pacmap, plot_labels_pacmap
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
-    ax, f = getSetup((12, 10), (5, 4))
+    ax, f = getSetup((12, 12), (6, 4))
     subplotLabel(ax)
 
     X = anndata.read_h5ad("/opt/pf2/thomson_fitted.h5ad")
+    
+    plot_labels_pacmap(X, "Cell Type", ax[0])
+    plot_labels_pacmap(X, "Cell Type2", ax[1])
+    
+    ax[2].axis("off")
+    ax[3].axis("off")
 
     for i in range(1, 21):
-        plot_wp_pacmap(X, i, ax[i - 1], cbarMax=0.3)
+        plot_wp_pacmap(X, i, ax[i + 3], cbarMax=0.3)
 
     return f
